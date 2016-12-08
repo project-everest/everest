f4cc9f7e4699c681ecd635ce3e8fcac407d64e37
Jonathan Protzenko <protz@microsoft.com>
2016-12-07 16:14:57 -0800

    Track branches as well to enable auto-update

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,12 @@
 declare -A hashes
-hashes[mitls-fstar]=64a55a7e098b1494373911bf15cfd8a5e7566120
-hashes[Spartan]=4622e0a7740525b37c64dfc9697a3aa6e7ebd9c4
-hashes[FStar]=de6f92d443e9a519faf0b160f91a13e20b3e5682
+declare -A branches
+hashes[mitls-fstar]=5c5165995a441cbefc133300865990e382029b87
+branches[mitls-fstar]=adl_lowlevel_merge
+hashes[Spartan]=346a2cc752be50932392b29a863d77a4c65dba37
+branches[Spartan]=master
+hashes[FStar]=9b55ac14ac7c5e19b9f46ad451faaffd2cb97fde
+branches[FStar]=c_record_aead
 hashes[hacl-star]=8b81fac33a95c43dbde3fae85fde93d1d3f0f0bb
-hashes[kremlin]=369a238ae5a9a923959cc7e80f05ff383970e267
+branches[hacl-star]=jk_low_level_crypto
+hashes[kremlin]=8d7aea4eb4a222b06e6503180e267c524a0d711c
+branches[kremlin]=master
