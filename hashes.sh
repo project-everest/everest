9e72c64d1482b01a3d0cc8404927dd54bc59fe6d
Dzomo, the everest Yak <everbld@microsoft.com>
2016-12-09 15:18:13 -0800

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -2,11 +2,11 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=a5c1ac5ba65879faf30a4bf654b6e6548cd3ab42
 branches[mitls-fstar]=adl_lowlevel_merge
-hashes[Spartan]=92852b66229ba1300abd1de1ad4863923e1d73da
+hashes[Spartan]=a8de4b00b52a46b685ece33d87195abb77b183dd
 branches[Spartan]=master
 hashes[FStar]=4b607c9160aa8fbee153b977d91f5e709ce11d85
 branches[FStar]=c_record_aead
-hashes[hacl-star]=8b81fac33a95c43dbde3fae85fde93d1d3f0f0bb
+hashes[hacl-star]=0bdd09e379b66d8016f3579b2af00e4072e17479
 branches[hacl-star]=jk_low_level_crypto
 hashes[kremlin]=6051d1b35d823175a75854d917259ca304c79c1d
 branches[kremlin]=master
