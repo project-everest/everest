49db7b27be93ae6b68d1ab5b4131be9d4e969f34
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2016-12-16 10:46:22 -0800

    New set of revisions

--- hashes.sh
+++ hashes.sh
@@ -1,10 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=94d932735d3c07b5a16933850d325e2965c9defa
+hashes[mitls-fstar]=20a0023dc3f5c0b470ee18cdfa83839a6a03f91e
 branches[mitls-fstar]=adl_lowlevel_merge
-hashes[Spartan]=0e468a84c72b23d2283dcf3df4ecc83103d559d7
+hashes[Spartan]=27f5e7a022a6f1516fc8440ebbf7b18fffd570f8
 branches[Spartan]=master
-hashes[FStar]=83f4e1906ff94886add178b3c785039968a579eb
+hashes[FStar]=c584e791464cb3c37a1d94ad845018ea454314eb
 branches[FStar]=c_record_aead
 hashes[hacl-star]=0bdd09e379b66d8016f3579b2af00e4072e17479
 branches[hacl-star]=jk_low_level_crypto
