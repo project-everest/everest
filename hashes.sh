d04d18e2b2f364e421b9cdd1a1d3806a4e0fd66a
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2017-01-06 17:45:36 +0000

    This should give a working Everest integration test, thanks Antoine

--- hashes.sh
+++ hashes.sh
@@ -1,10 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=01bc100d480c7ce7995a64ab06dc63c6209f7433
+hashes[mitls-fstar]=08cdf202340eac17861ad41990abc2b6ac1ff674
 branches[mitls-fstar]=adl_lowlevel_merge
 hashes[Spartan]=8560d5a099d81c532c1c79356a2116757bd7fe4e
 branches[Spartan]=master
-hashes[FStar]=15dfdca0c5b07eade5c9d26a747d4f13cedf4efa
+hashes[FStar]=c53457565578f7242a57128a9fa13242cc4f421e
 branches[FStar]=master
 hashes[hacl-star]=53bb1319ab9fe5f5c92ff408a3a136ee831a30dd
 branches[hacl-star]=jk_dev
