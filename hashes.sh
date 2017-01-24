ef7ad82ffa06676372068afd121cb7702584f6c2
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2017-01-23 16:46:21 -0800

    Switch to each project's master branch

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=870e801876941582df0c6c2e8b068cbf5890dff8
-branches[mitls-fstar]=adl_lowlevel_merge
+branches[mitls-fstar]=master
 hashes[Spartan]=bcf1c2be6d4d98ec86d8360a95c1578bb03cae99
 branches[Spartan]=master
 hashes[FStar]=b31dbb82df176275c5eadfe7edfcf0df65d61fdd
 branches[FStar]=master
 hashes[hacl-star]=53bb1319ab9fe5f5c92ff408a3a136ee831a30dd
-branches[hacl-star]=jk_dev
+branches[hacl-star]=master
 hashes[kremlin]=5e43104de9bc9952507c9bc7010eaa6780dc46fb
 branches[kremlin]=master
