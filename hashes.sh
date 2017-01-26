821961d1717a3cf2f527d572a3989d6086ab3cf3
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2017-01-26 21:30:59 +0100

    Changing scripts to use Vale instead of Spartan

--- hashes.sh
+++ hashes.sh
@@ -2,8 +2,8 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=ae86a3725d16ab52d608d8dc1fb4b691ff1686fa
 branches[mitls-fstar]=master
-hashes[Spartan]=bcf1c2be6d4d98ec86d8360a95c1578bb03cae99
-branches[Spartan]=master
+hashes[Vale]=71aed6df5733790ac2b5f63eb6fa61ffe4da3f54
+branches[Vale]=master
 hashes[FStar]=5ff3bc6ec14ac96ec25a5c6289a1bf0f215e3e42
 branches[FStar]=master
 hashes[hacl-star]=c271cb11746c69068af7833b85a187480d09eb48
--- repositories.sh
+++ repositories.sh
@@ -4,4 +4,4 @@ repositories[FStar]=git@github.com:FStarLang/FStar.git
 repositories[kremlin]=git@github.com:FStarLang/kremlin.git
 repositories[hacl-star]=git@github.com:mitls/hacl-star.git
 repositories[mitls-fstar]=git@github.com:mitls/mitls-fstar.git
-repositories[Spartan]=ssh://msresearch@msresearch.visualstudio.com/DefaultCollection/Ironclad/_git/Spartan
+repositories[Vale]=git@github.com:project-everest/vale.git
