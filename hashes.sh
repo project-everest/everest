ff2e02c3920b55b9d613937de35d91835c1faa5f
Benjamin Beurdouche <benjamin.beurdouche@inria.fr>
2017-01-28 10:25:11 +0100

    Move vale project identifier to lowercase

--- hashes.sh
+++ hashes.sh
@@ -2,8 +2,8 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=ae86a3725d16ab52d608d8dc1fb4b691ff1686fa
 branches[mitls-fstar]=master
-hashes[Vale]=71aed6df5733790ac2b5f63eb6fa61ffe4da3f54
-branches[Vale]=master
+hashes[vale]=71aed6df5733790ac2b5f63eb6fa61ffe4da3f54
+branches[vale]=master
 hashes[FStar]=c545db407fa16a2a1a533717abb1438c58e793b0
 branches[FStar]=master
 hashes[hacl-star]=c271cb11746c69068af7833b85a187480d09eb48
