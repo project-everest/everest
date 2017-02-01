a1bac5800b565e46042fcabd74c324d355b68c6b
Tahina Ramananandro <taramana@microsoft.com>
2017-01-31 23:54:28 -0800

    Point every project to its master branch
    Commit hashes remain set to the latest working set of commits in
    taramana_include_seq_lists, which are now merged into their
    respective master branches.

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
 hashes[vale]=e783004993829397955241d49b41ec0ee97c3560
-branches[vale]=taramana_include_seq
+branches[vale]=master
 hashes[mitls-fstar]=bd58f0f38ca3423d8d3c694d5e9a2b88a8e15a0b
-branches[mitls-fstar]=taramana_include_seq_lists
+branches[mitls-fstar]=master
 hashes[FStar]=89a9007dacd675c16c012bbc5f6dda31a5698a26
-branches[FStar]=taramana_include_seq_lists
+branches[FStar]=master
 hashes[hacl-star]=b11e54af453ab3ba01a21fe7f39b21a571732c84
-branches[hacl-star]=taramana_include_seq_lists
+branches[hacl-star]=master
 hashes[kremlin]=5be326704017f27077501e5f0ec56ba41730b234
-branches[kremlin]=taramana_include_seq
+branches[kremlin]=master
