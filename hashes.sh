991f2621194fa8cb8b1ff60aca548102c0caa05e
Tahina Ramananandro <taramana@microsoft.com>
2017-03-01 11:35:06 -0800

    F*, miTLS: nits in build processes

--- hashes.sh
+++ hashes.sh
@@ -2,9 +2,9 @@ declare -A hashes
 declare -A branches
 hashes[vale]=330f092d77acbc4f2689388522efb0b3d5d5ed08
 branches[vale]=master
-hashes[mitls-fstar]=3a827a98b79dfd1e31a63dc707f0814c341ca7d3
+hashes[mitls-fstar]=7cf3362b759e3ae703e20ecce32e72cc41d8d30f
 branches[mitls-fstar]=master
-hashes[FStar]=c3d1a3b633c241cc378431dd340be6922950a972
+hashes[FStar]=5db98f74ea7073b5c7e7c622de57acda06906719
 branches[FStar]=master
 hashes[hacl-star]=f0d027ecaaa6d3f19f2d451e201c04092a10c874
 branches[hacl-star]=master
