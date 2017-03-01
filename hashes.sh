0e78cd4e86580c279d5b3770d8b0a7dfa92cd852
Tahina Ramananandro <taramana@microsoft.com>
2017-03-01 15:47:03 -0800

    miTLS: make `verify` work again
    At this point, Everest verifies on Ubuntu 16.04

--- hashes.sh
+++ hashes.sh
@@ -2,9 +2,9 @@ declare -A hashes
 declare -A branches
 hashes[vale]=330f092d77acbc4f2689388522efb0b3d5d5ed08
 branches[vale]=master
-hashes[mitls-fstar]=7cf3362b759e3ae703e20ecce32e72cc41d8d30f
+hashes[mitls-fstar]=ff2a53df8b25913080444bb45d819653b86c2a35
 branches[mitls-fstar]=master
-hashes[FStar]=5db98f74ea7073b5c7e7c622de57acda06906719
+hashes[FStar]=e43fa6af028b8be6baee0410cf6b964934ad65b1
 branches[FStar]=master
 hashes[hacl-star]=f0d027ecaaa6d3f19f2d451e201c04092a10c874
 branches[hacl-star]=master
