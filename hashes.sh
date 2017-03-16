1e966c591293ffdf5535d10b987622f517c2a57f
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2017-03-15 21:29:47 -0700

    See how much breaks

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[vale]=0b6e605237f12fe2fb22d8fecb27142ecd1132fe
-branches[vale]=master
 hashes[mitls-fstar]=17bf1bc60a79da9dff7802873f4fe6a56de03d07
 branches[mitls-fstar]=master
-hashes[FStar]=e341e6178038e8367835b0e387b0f7bbd6c0d884
-branches[FStar]=master
-hashes[hacl-star]=85a605e4685d1d124416f68633b7e23d7e7301d5
+hashes[vale]=0b6e605237f12fe2fb22d8fecb27142ecd1132fe
+branches[vale]=master
+hashes[FStar]=337feb92191a2f8b62509c534527f297f83f9efd
+branches[FStar]=protz_tany
+hashes[hacl-star]=c0b1d91e096e03201cca97e458bed8c53223f37a
 branches[hacl-star]=master
-hashes[kremlin]=861468cb2dcebabc24b00aebd95b28a53d83ef97
+hashes[kremlin]=5806430b6ff44973b65af8e6c394e10fe757eefb
 branches[kremlin]=master
