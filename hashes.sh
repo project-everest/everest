cfc691d3aa9dfbe5aff3778ec7a6b4a38b1c085e
Dzomo the everest Yak <everbld@microsoft.com>
2017-03-20 02:44:50 -0700

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -1,10 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[vale]=59c2349363201e622bdbc32de795cfd9dc1be13a
+hashes[vale]=ce9e51da688de2dd1f488c253dc69d13569ee398
 branches[vale]=master
-hashes[mitls-fstar]=78e64f4936ca0560d6be5db46a6fce7917f9b4c0
+hashes[mitls-fstar]=6b402a9cc53854164ef1bd8d55811e4c9edbfb23
 branches[mitls-fstar]=master
-hashes[FStar]=e960f3c988b33e1c882abc7461a79dd82236f4ec
+hashes[FStar]=817ecd9ffb5d7c5e797bb635cefb5c2fdf5ed6f2
 branches[FStar]=master
 hashes[hacl-star]=0708e8cbd3cc337b570591d33664980b4ff99a68
 branches[hacl-star]=master
