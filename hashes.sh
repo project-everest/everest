6d14598ba47ca7d73c59a64db1ec494af98fa9af
Santiago Zanella-Beguelin <santiago@microsoft.com>
2017-04-21 14:18:55 +0100

    Update mitls-fstar

--- hashes.sh
+++ hashes.sh
@@ -2,7 +2,7 @@ declare -A hashes
 declare -A branches
 hashes[vale]=8af38afa9ff1f093ba100e3e470462e5f20ca2b1
 branches[vale]=master
-hashes[mitls-fstar]=c6c2f4f1ac1ce4ee552aadf2b091babed6c40396
+hashes[mitls-fstar]=304a48163f1d1fc1de3598ec55e290088e6e1c19
 branches[mitls-fstar]=mitls_handshake
 hashes[FStar]=16a7f60963bf4aa15a01cb97c3ed399111e3266b
 branches[FStar]=master
