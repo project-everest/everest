fbcf5ec9a9815e0167b3211a751bf2da78893a0f
Santiago Zanella-Beguelin <santiago@microsoft.com>
2017-07-13 14:41:25 +0100

    Update

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=4cee62f407add9a351cea0bdd03513382704194d
+hashes[mitls-fstar]=e4116e0374af2d05ebf855613e4f2a07f7df6e53
 branches[mitls-fstar]=mitls_handshake
 hashes[vale]=c0790d7b84b3e003755f61ae1c6bab6861a8cab2
 branches[vale]=master
 hashes[FStar]=b645c45e51476920fa94b8c03cd2f8a569537fa7
 branches[FStar]=master
-hashes[hacl-star]=937b1b2fdff4ca06b4d0d4d5adc61353116407bc
+hashes[hacl-star]=050c0670f2adb7dce8bfa19a1f4db705d0f09853
 branches[hacl-star]=migrating_sts_bis
 hashes[kremlin]=92c51c9695157ef31538334dae4788a1592e7a69
 branches[kremlin]=master
