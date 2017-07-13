4131da2fdd92f3e8d7604bc1ca6e7fd6f0a895bf
Santiago Zanella-Beguelin <santiago@microsoft.com>
2017-07-13 11:35:14 +0100

    Udpate hashes

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=819f6c4df440c5d956f211eea8be73719af261af
-branches[mitls-fstar]=master
-hashes[vale]=3b55394035a0f456b29db52e6fcc8bcc8a7e76d4
+hashes[mitls-fstar]=4cee62f407add9a351cea0bdd03513382704194d
+branches[mitls-fstar]=mitls_handshake
+hashes[vale]=c0790d7b84b3e003755f61ae1c6bab6861a8cab2
 branches[vale]=master
-hashes[FStar]=9647d244894ff517065d9b6bfd4c5be4e2061f07
+hashes[FStar]=b645c45e51476920fa94b8c03cd2f8a569537fa7
 branches[FStar]=master
-hashes[hacl-star]=20af4bec47ec13120d48d04ba63028b6bb459829
+hashes[hacl-star]=39f51439e91bfe53a5c79933d941a26ce01eed7c
 branches[hacl-star]=migrating_sts_bis
-hashes[kremlin]=4251314e4f76420c77f7eba90c5c94b7a18e1ca5
+hashes[kremlin]=92c51c9695157ef31538334dae4788a1592e7a69
 branches[kremlin]=master
