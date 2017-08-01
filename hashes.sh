acc3654f4a60ccd1c526d0853b0db29ffe551c85
Tahina Ramananandro <taramana@microsoft.com>
2017-08-01 15:15:13 -0700

    Create new c_mitls2c branches

--- hashes.sh
+++ hashes.sh
@@ -3,9 +3,9 @@ declare -A branches
 hashes[vale]=f6c33caf1ffc8291e7baaace8efe2e4a4e741926
 branches[vale]=master
 hashes[mitls-fstar]=268e61b3d1e724cbf1f3bf78f45f30ee02f6f355
-branches[mitls-fstar]=guido_test
+branches[mitls-fstar]=c_mitls2c
 hashes[FStar]=6cafdc1633b1f8007186b0dc3aa90fcecbe2875d
-branches[FStar]=taramana_pointers_with_codes
+branches[FStar]=c_mitls2c
 hashes[hacl-star]=5245e130263be432b85694fbf2af67a3815d56e9
 branches[hacl-star]=guido_test
 hashes[kremlin]=62622db8ed994556ed77e6e6861229703aaf8432
