a6eb381dae4dedd43c23fe98892fa8359e867684
Tahina Ramananandro <taramana@microsoft.com>
2017-08-01 14:48:55 -0700

    Latest hashes: F* and kreMLin master, HACL* and miTLS guido_test

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=0f36db85994c7a6129547880775b8caef5767548
-branches[mitls-fstar]=master
 hashes[vale]=f6c33caf1ffc8291e7baaace8efe2e4a4e741926
 branches[vale]=master
-hashes[FStar]=d71792127
+hashes[mitls-fstar]=268e61b3d1e724cbf1f3bf78f45f30ee02f6f355
+branches[mitls-fstar]=guido_test
+hashes[FStar]=89b6a1862aa74cedc42aadbb17fbaed1c9653bbd
 branches[FStar]=master
-hashes[hacl-star]=784d3b26
-branches[hacl-star]=master
-hashes[kremlin]=92c51c9695157ef31538334dae4788a1592e7a69
+hashes[hacl-star]=5245e130263be432b85694fbf2af67a3815d56e9
+branches[hacl-star]=guido_test
+hashes[kremlin]=f3a8fbf58a713ab5548902e5272bd9dd66688630
 branches[kremlin]=master
