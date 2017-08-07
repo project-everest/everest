a7c172547a73fdfe7a76901f999818808b66656e
Jared Roesch <roeschinc@gmail.com>
2017-08-07 13:27:49 -0700

    Bump snapshot

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
+hashes[mitls-fstar]=cc02da5f76b8740d839a816378f45ab0b8ceb812
+branches[mitls-fstar]=c_mitls2c
 hashes[vale]=f6c33caf1ffc8291e7baaace8efe2e4a4e741926
 branches[vale]=master
-hashes[mitls-fstar]=268e61b3d1e724cbf1f3bf78f45f30ee02f6f355
-branches[mitls-fstar]=c_mitls2c
 hashes[FStar]=a97b239199029399416a0a9a4ec89090debd6e30
 branches[FStar]=c_mitls2c
 hashes[hacl-star]=5245e130263be432b85694fbf2af67a3815d56e9
 branches[hacl-star]=guido_test
-hashes[kremlin]=62622db8ed994556ed77e6e6861229703aaf8432
+hashes[kremlin]=2cfdf15b16ef30887346ec1b001f60ef2339d797
 branches[kremlin]=c_mitls2c
