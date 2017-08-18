574d00b08fecaa002a5565556d4fb48a8ab0e06c
Santiago Zanella-Beguelin <santiago@microsoft.com>
2017-08-18 16:26:53 +0100

    Use stable branches

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=eb1800f5ab3b3a5928c82da237e2414af7c2b620
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=f8c469086dda8f2d5ef08d9090f85492619a35cf
+branches[mitls-fstar]=stable
 hashes[vale]=4ff7724e3c98e27157c5fc247c8d2434f8e2a404
-branches[vale]=master
+branches[vale]=stable
 hashes[FStar]=1caf1bc11fa3cba9e8bbc5a902055fb0083be1d6
-branches[FStar]=master
-hashes[hacl-star]=bab05f3cb2def61ff6fe62be454d170abc9618db
-branches[hacl-star]=master
+branches[FStar]=stable
+hashes[hacl-star]=9bf465524fccd0c650ad5b39790113219b82f9f3
+branches[hacl-star]=stable
 hashes[kremlin]=32c177d6622badce550aa08c1158f8b824480531
-branches[kremlin]=master
+branches[kremlin]=stable
