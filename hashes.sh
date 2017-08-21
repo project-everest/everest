39434a1e8f3490e2e16c8fecbcccc9c4f2a19925
Jared Roesch <roeschinc@gmail.com>
2017-08-21 16:15:27 -0700

    Snapshot with new README

--- hashes.sh
+++ hashes.sh
@@ -1,10 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=19c755d93542bc721ce3c471c91a9c8c640297ab
+hashes[mitls-fstar]=e8a6b7387cac0352092893b49ab03f89ca1ac302
 branches[mitls-fstar]=c_mitls2c
 hashes[vale]=f6c33caf1ffc8291e7baaace8efe2e4a4e741926
 branches[vale]=master
-hashes[FStar]=ba09f33f06c268160c5b4f084ac5b8b8628a18a0
+hashes[FStar]=2772dcbfb5d5caf761957d3632b75f2b1cf52005
 branches[FStar]=c_mitls2c
 hashes[hacl-star]=9bf465524fccd0c650ad5b39790113219b82f9f3
 branches[hacl-star]=stable
