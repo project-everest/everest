9a765140f51299f5b600e372055e7d3f65ccd1c1
Tahina Ramananandro <taramana@microsoft.com>
2017-08-23 11:49:52 -0700

    Temporary miTLS printf debug (with @BarryBo)
    ***NO_CI***

--- hashes.sh
+++ hashes.sh
@@ -1,9 +1,9 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=51a9b6e55e3590bd0a40d55acf372113b5b65efe
-branches[mitls-fstar]=master
 hashes[vale]=4ff7724e3c98e27157c5fc247c8d2434f8e2a404
 branches[vale]=master
+hashes[mitls-fstar]=746fa888d17096224eae8da21699987ae8d7bdfb
+branches[mitls-fstar]=master
 hashes[FStar]=1d5c58e59e4484d17f589b4944138df751b821fb
 branches[FStar]=master
 hashes[hacl-star]=239126fc9cae2241b1df053e63ed7cbd54e77c17
