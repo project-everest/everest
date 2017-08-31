5a832d6a4726775403743454f8ac248417f7c28d
Jared Roesch <roeschinc@gmail.com>
2017-08-31 15:28:23 -0700

    Snapshot

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=59934b0c93fc59209f3ee33736e04a11eeb58f70
-branches[mitls-fstar]=master
-hashes[vale]=4ff7724e3c98e27157c5fc247c8d2434f8e2a404
+hashes[mitls-fstar]=4cb362316bc6af06cd3594ea767a3129e1bcaa40
+branches[mitls-fstar]=c_mitls2c
+hashes[vale]=f6c33caf1ffc8291e7baaace8efe2e4a4e741926
 branches[vale]=master
-hashes[FStar]=ebb070698408a37891691c78e2a48ba0bf85076a
-branches[FStar]=master
+hashes[FStar]=6ea462a483ff8b0c87f589f0eba0a75051019ed6
+branches[FStar]=tchajed_kv_parsing
 hashes[hacl-star]=d9bf9adafb0fe5feabfddca8f65e738a5b0eec62
 branches[hacl-star]=master
-hashes[kremlin]=812bf718d3d086ad551dd7ac3e533d2b38ba1e35
-branches[kremlin]=master
+hashes[kremlin]=c79c9f483d895b02a2ebb40cbc9fa6067cc8cb2b
+branches[kremlin]=schema_21
