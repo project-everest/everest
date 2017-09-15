6b3073457f9f529453ecd1f97ef4d6a28bf1cb40
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2017-09-15 14:31:34 -0400

    try again

--- hashes.sh
+++ hashes.sh
@@ -8,5 +8,5 @@ hashes[FStar]=d4664d59ba4e06e6d3be4f187ec4ef1778908e34
 branches[FStar]=c_mitls2c_new_attrs
 hashes[hacl-star]=05db0a0eea76b0179f73152f941853bb9cc4b0e4
 branches[hacl-star]=c_mitls2c_new_attrs
-hashes[kremlin]=7f4675f6f00ba4f9fe5de5e286be886224f56c92
+hashes[kremlin]=9bc9e100bf58f6b472cff5e6aaa9ceb417f68bb8
 branches[kremlin]=c_mitls2c_new_attrs
