0500c372a14cfb4a0abc6e59d17c57e7c1b52c4f
Jared Roesch <roeschinc@gmail.com>
2017-09-14 22:07:20 -0700

    snap

--- hashes.sh
+++ hashes.sh
@@ -2,11 +2,11 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=69f1592f56e34f640dc984baf7b7a53a3e0373fe
 branches[mitls-fstar]=master
-hashes[vale]=f6c33caf1ffc8291e7baaace8efe2e4a4e741926
-branches[vale]=master
+hashes[vale]=b58a29b94c145237b84509c323bdb4bc855d448a
+branches[vale]=kremlin
 hashes[FStar]=d4664d59ba4e06e6d3be4f187ec4ef1778908e34
 branches[FStar]=c_mitls2c_new_attrs
 hashes[hacl-star]=05db0a0eea76b0179f73152f941853bb9cc4b0e4
 branches[hacl-star]=c_mitls2c_new_attrs
-hashes[kremlin]=7a4fb0849c8ca963f979ae0dba7bd4ea28c96e2f
+hashes[kremlin]=be2898f55d7ee7973eb2c793aeea27a76e61e77e
 branches[kremlin]=c_mitls2c_new_attrs
