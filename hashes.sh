3ffb7d0e200764faf779bee862a278293658fdcb
Jared Roesch <roeschinc@gmail.com>
2017-09-18 13:01:04 -0700

    snapshot w/ c_mitls2c_new_attrs work

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=e8a6b7387cac0352092893b49ab03f89ca1ac302
+hashes[mitls-fstar]=a25575661c958b6f585369de30f4c75882f696a0
 branches[mitls-fstar]=c_mitls2c
-hashes[vale]=f6c33caf1ffc8291e7baaace8efe2e4a4e741926
-branches[vale]=master
-hashes[FStar]=2772dcbfb5d5caf761957d3632b75f2b1cf52005
+hashes[vale]=b58a29b94c145237b84509c323bdb4bc855d448a
+branches[vale]=kremlin
+hashes[FStar]=71d774c8b45ea6971652968261835c291058e843
 branches[FStar]=c_mitls2c
-hashes[hacl-star]=9bf465524fccd0c650ad5b39790113219b82f9f3
-branches[hacl-star]=stable
-hashes[kremlin]=db63f97fa6dfc875731f3c0542b2b9da63a6fe20
+hashes[hacl-star]=073f7036e8e94e5ad07351fc53abf9d64eef4b0c
+branches[hacl-star]=c_mitls2c
+hashes[kremlin]=0fb60a6e0639badd7c5fda1e839383787953ebca
 branches[kremlin]=c_mitls2c
