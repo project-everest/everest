80184c0aca575d84381e1b545de6d82369356a81
Jonathan Protzenko <protz@microsoft.com>
2017-09-22 07:24:32 -0700

    Make sure everyone is on master

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=0e5d97fc3222ece7c41fd3893231ca32e8e55a16
-branches[mitls-fstar]=c_mitls2c_new_attrs
+branches[mitls-fstar]=master
 hashes[vale]=4de3dd7830977d2cd3e2dd8984dd5d14f4484879
-branches[vale]=kremlin
+branches[vale]=master
 hashes[FStar]=5d6ca3afbdcb79db04cc29ef13d3d426244ecab4
-branches[FStar]=c_mitls2c_new_attrs
+branches[FStar]=master
 hashes[hacl-star]=c7b35016f73750e296fc3ec1278fa5f6b84e3188
-branches[hacl-star]=c_mitls2c_new_attrs
+branches[hacl-star]=master
 hashes[kremlin]=5b5564e83dde2a7c7f5637ffbd0b6c9ff1d9f330
-branches[kremlin]=c_mitls2c_new_attrs
+branches[kremlin]=master
