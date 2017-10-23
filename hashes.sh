7dfbd238f6cee005f8aa728f80dd9844e9168e4c
Nikhil Swamy <nikswamy@users.noreply.github.com>
2017-10-23 13:01:11 -0700

    new hashes

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=7cfff243a965fe849045586ee402c99f500779d5
+hashes[mitls-fstar]=97a918c1c905f1c17dbe55857e4875cc1ba1b576
 branches[mitls-fstar]=c_mitls2c
 hashes[vale]=b58a29b94c145237b84509c323bdb4bc855d448a
 branches[vale]=kremlin
-hashes[FStar]=420e3f047068560abafdf979cbc25982229cde5e
+hashes[FStar]=1a9f6aa869b7f6010be73266fe969f5324ce7ba0
 branches[FStar]=c_mitls2c
 hashes[hacl-star]=d594607131232c96b736b36d46c872fbedfadf16
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=fd2f81c84e0419f28ee3fc254bdd43d53f98ce87
+hashes[kremlin]=fe448ec75ac51b1ac1b6444bbb8f015125e0b50e
 branches[kremlin]=c_mitls2c
