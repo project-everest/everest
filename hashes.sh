aa63c7bec0bd05470a4b13c7039a03d4ebae0f5a
Tahina Ramananandro <taramana@microsoft.com>
2017-11-01 13:47:11 -0700

    Advance KreMLin, required by HACL*

--- hashes.sh
+++ hashes.sh
@@ -8,5 +8,5 @@ hashes[FStar]=39e78bce667b434d039d6fe0de7165393af0d532
 branches[FStar]=stable
 hashes[hacl-star]=be5753bad76d7974aaaa32c461d002e7cc4c12e7
 branches[hacl-star]=master
-hashes[kremlin]=4f495c3aee1b02d909f0b0bbb549825847a83cd9
+hashes[kremlin]=824cabad8dc87c86ae19d3d30a8e21201e0008da
 branches[kremlin]=master
