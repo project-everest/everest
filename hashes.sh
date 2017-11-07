5db5d17bc19bc8cf062ecf1496a8d57ee01ef7da
Nikhil Swamy <nikswamy@users.noreply.github.com>
2017-11-06 17:20:48 -0800

    bump forward, after upgrading c_mitls2c in F*

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=444670e6baca465a0be434967cb94d703e4c1547
+hashes[mitls-fstar]=cc9aa859408f380a0a78288ccdb1007adf3c51c7
 branches[mitls-fstar]=c_mitls2c
 hashes[vale]=b58a29b94c145237b84509c323bdb4bc855d448a
 branches[vale]=kremlin
-hashes[FStar]=1a9f6aa869b7f6010be73266fe969f5324ce7ba0
+hashes[FStar]=296113bd9119473a7199fed1e65d622870085f9d
 branches[FStar]=c_mitls2c
-hashes[hacl-star]=d594607131232c96b736b36d46c872fbedfadf16
+hashes[hacl-star]=67cb7db8bfe2cacf18ff9a3108a0443a05a99f18
 branches[hacl-star]=c_mitls2c
 hashes[kremlin]=5ea4f3861f319d15bb3f6c0f8b276092755aeb45
 branches[kremlin]=c_mitls2c
