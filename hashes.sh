616003307eda1fc0132cd5adb43c26c0f18c312a
Nikhil Swamy <nikswamy@users.noreply.github.com>
2017-11-09 15:42:21 -0800

    bump hashes; separate compilation in mitls

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=cc9aa859408f380a0a78288ccdb1007adf3c51c7
+hashes[mitls-fstar]=cb55924c73ee0d6d6d31954b94496a0553eb3777
 branches[mitls-fstar]=c_mitls2c
 hashes[vale]=b58a29b94c145237b84509c323bdb4bc855d448a
 branches[vale]=kremlin
-hashes[FStar]=296113bd9119473a7199fed1e65d622870085f9d
+hashes[FStar]=3ce3be1245af2328d856db0a851868499f59f6e4
 branches[FStar]=c_mitls2c
-hashes[hacl-star]=67cb7db8bfe2cacf18ff9a3108a0443a05a99f18
+hashes[hacl-star]=9a36ed903a65927f9ec6ed32f5b1f26259ff76b0
 branches[hacl-star]=c_mitls2c
 hashes[kremlin]=5ea4f3861f319d15bb3f6c0f8b276092755aeb45
 branches[kremlin]=c_mitls2c
