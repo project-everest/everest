48495ccc285a6038bd462ba33c2262c42d216953
Nikhil Swamy <nikswamy@users.noreply.github.com>
2017-11-18 16:17:26 -0800

    bump hashes

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=d429a7f461f20e22e67385023cd63a4a63635cb3
+hashes[mitls-fstar]=07cff20a984de91790e013703020dda7cfe60219
 branches[mitls-fstar]=c_mitls2c
 hashes[vale]=1038809b5a67ef5e0e18f403f32dc9cb991f325e
 branches[vale]=kremlin
-hashes[FStar]=9e2d70cf68070ba2d55c224db141176d7baf1b4d
+hashes[FStar]=1c54a7ad221bce0c6a1f2887d00eb613765f9478
 branches[FStar]=master
 hashes[hacl-star]=f946f580058fc46028cf40d557bcb185ede07b59
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=305062b0e9c8d848186fc17d1d86663d67f87a75
+hashes[kremlin]=c13fb00b5ec9b71f3464e4623fdff714a3aedc2a
 branches[kremlin]=nik_mitls2c
