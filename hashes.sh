18b2c01a8ab000646e61d1075c3632be4b76090e
Nikhil Swamy <nikswamy@users.noreply.github.com>
2017-12-18 12:47:36 -0800

    new hashes for the quic2c work

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=1f4dc695107206e6f6a36cf5fd0137266fa6db9a
-branches[mitls-fstar]=c_mitls2c
+hashes[mitls-fstar]=34774b12b133c09b5a1dcbf5c10290629daf5818
+branches[mitls-fstar]=quic2c
 hashes[vale]=fa2e31b8f44debcc43a1d8ec0f6fdeecb2ce6782
 branches[vale]=master
-hashes[FStar]=5dcf0bac328144324024ea31786508e20da5fa8d
-branches[FStar]=c_mitls2c
-hashes[hacl-star]=f12d4e2e38f4a8e3bb54de2f160f77641b7b29d6
+hashes[FStar]=ef3405e220a16534bb233b394abc0eb6fe93a85b
+branches[FStar]=master
+hashes[hacl-star]=9205e926594eb7d3ad28b90318d7bccb91fde646
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=de9df83c0146bc54717721766fb6a84394a1ab32
+hashes[kremlin]=1560363de5eb2b470998a53c285f92ff72aadfe3
 branches[kremlin]=fstar-master
