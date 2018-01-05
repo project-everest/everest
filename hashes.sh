c9bdeac572830f645714f4d6e76f9103bf70360f
Nikhil Swamy <nikswamy@users.noreply.github.com>
2018-01-05 15:14:00 -0800

    new hashes to produce mitls.cmxa

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=34774b12b133c09b5a1dcbf5c10290629daf5818
+hashes[mitls-fstar]=ee92cc52e004928278120452de276f5841ae7231
 branches[mitls-fstar]=quic2c
 hashes[vale]=fa2e31b8f44debcc43a1d8ec0f6fdeecb2ce6782
 branches[vale]=master
-hashes[FStar]=ef3405e220a16534bb233b394abc0eb6fe93a85b
+hashes[FStar]=5341c55425f14abbe7718abca865f22984a54f93
 branches[FStar]=master
-hashes[hacl-star]=9205e926594eb7d3ad28b90318d7bccb91fde646
+hashes[hacl-star]=18fbe6cfef038f7af98df7106fa8bdf399d5e869
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=1560363de5eb2b470998a53c285f92ff72aadfe3
+hashes[kremlin]=5eb0e11d4a5c3cfd1fc2045f2b25aeb4dd1491d0
 branches[kremlin]=fstar-master
