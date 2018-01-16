d7982b970eb94e8cd63a29f8541f68bfa0011aef
Nikhil Swamy <nikswamy@users.noreply.github.com>
2018-01-16 15:40:31 -0800

    mitls.exe runs

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=37897ea90226d2ac250a73753219c3e5094ec754
+hashes[mitls-fstar]=b2d89b75f9e09a0fe242f24a31163f92e23fcf66
 branches[mitls-fstar]=quic2c
 hashes[vale]=a80cbd5c8b23905180a5311422cb75f66ec7670d
 branches[vale]=fstar
-hashes[FStar]=246723f9b7dc3b180a227813b92b7155d037c2cb
-branches[FStar]=master
-hashes[hacl-star]=328d78af01b69b91af3c119492edc7bfef9ed7a3
-branches[hacl-star]=c_mitls2c
-hashes[kremlin]=ca5fbf454ffb4abd409c138bad629c23481c24ca
-branches[kremlin]=fstar-master
+hashes[FStar]=132a95d33abbfdd97c47155097a397e92a84af0e
+branches[FStar]=c_mitls2c
 hashes[MLCrypto]=62c875e69565595911f4bed27c2172c575258a98
 branches[MLCrypto]=master
+hashes[hacl-star]=51e973d3c6e89bfc559febd67aca0d82ae33b6bb
+branches[hacl-star]=c_mitls2c
+hashes[kremlin]=d9dc64859a4c38790eee19e13355205183ee7c84
+branches[kremlin]=fstar-master
