d60cbc5177bdccb9befe9d13ba2e63c12cfa11f5
Nikhil Swamy <nikswamy@users.noreply.github.com>
2018-01-19 23:12:37 -0800

    snap

--- hashes.sh
+++ hashes.sh
@@ -1,10 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=339b1ea22678258d8c95ed8a10c7d74b89aba228
+hashes[mitls-fstar]=664ba093b10ac7b4d5730b08eb3b9acbeda259c3
 branches[mitls-fstar]=quic2c
 hashes[vale]=a80cbd5c8b23905180a5311422cb75f66ec7670d
 branches[vale]=fstar
-hashes[FStar]=4156ddb3a0687d5f4485e6bd29d97ae754ba6a9c
+hashes[FStar]=a3b7fc764d019aa5216be9310a533afdbcc6953d
 branches[FStar]=master
 hashes[MLCrypto]=62c875e69565595911f4bed27c2172c575258a98
 branches[MLCrypto]=master
