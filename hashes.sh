af1f80ff827315211d2b0bbf5d9d39e703630ee3
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-01-26 12:33:04 -0800

    New hashes but everest ci is broken

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=d59a61447e0c18771ba103ef8ad4473b42317e59
+hashes[mitls-fstar]=1f29c6a42419fbea8c57019d1212b40e67892bed
 branches[mitls-fstar]=quic2c
 hashes[vale]=a80cbd5c8b23905180a5311422cb75f66ec7670d
 branches[vale]=fstar
-hashes[FStar]=9de9456e645ac24b4d52116cbb6ee1b9c482b1e3
-branches[FStar]=c_mitls2c
+hashes[FStar]=9b43c4c58042dcf3096adaaeb9a4c109eba1afe3
+branches[FStar]=master
 hashes[MLCrypto]=62c875e69565595911f4bed27c2172c575258a98
 branches[MLCrypto]=master
-hashes[hacl-star]=d5720b650007470a5255d47340ce233462abaad4
+hashes[hacl-star]=27a9eee8c5e46bd5c046fe7dd2ddeaa8b9a4cf2c
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=05865e95ae3393461fbe22eef72655dffae19a54
+hashes[kremlin]=743bb78efb36de308ca3fc5f52ed169b9eaa6fc0
 branches[kremlin]=fstar-master
