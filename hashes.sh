1150cac4811f5d4fea99757bba32f43155a868c9
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-01-29 10:13:00 -0800

    This gave me a successful build

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=e74c1099d4bf7e0de32249e92ad486fdeda5e918
+hashes[mitls-fstar]=d84562b7502185b965b36d24a37940971e0fb4c3
 branches[mitls-fstar]=quic2c
-hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
+hashes[vale]=fa2e31b8f44debcc43a1d8ec0f6fdeecb2ce6782
 branches[vale]=master
-hashes[FStar]=9de9456e645ac24b4d52116cbb6ee1b9c482b1e3
+hashes[FStar]=9b6139f29a2bb781773a9b025ee969c52bbac3ba
 branches[FStar]=master
 hashes[MLCrypto]=29ecaa33e3c3cafb81c737d1acfd5eff8b278bd3
 branches[MLCrypto]=master
-hashes[hacl-star]=dd8b93debb9bbfe1cbb3f59dbd89a4a986aed14d
+hashes[hacl-star]=5e7b53beb6b316a703b1b0ae723dec02e1803b91
 branches[hacl-star]=c_mitls2c
 hashes[kremlin]=743bb78efb36de308ca3fc5f52ed169b9eaa6fc0
 branches[kremlin]=fstar-master
