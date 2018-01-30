5b623cd7cd5d0722b17de26ddb14e9187278d066
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-01-30 10:01:24 -0800

    New hashes, fixes Antoine's problem

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=e5cf33abf143551fbefe826c27af31054b8d7ccc
+hashes[mitls-fstar]=ee53ee74832f12a20d8aeecc377a84b1a8f95db6
 branches[mitls-fstar]=quic2c
-hashes[vale]=518d9c5b9ec04a6cecce32404f9162d81f283050
-branches[vale]=fstar
-hashes[FStar]=fae99da60b2e47020ed7426aec5bc2606e1ddeb9
+hashes[vale]=fa2e31b8f44debcc43a1d8ec0f6fdeecb2ce6782
+branches[vale]=master
+hashes[FStar]=e287c00e95afa305d101ec4c115944e5f14c0dfb
 branches[FStar]=master
 hashes[MLCrypto]=29ecaa33e3c3cafb81c737d1acfd5eff8b278bd3
 branches[MLCrypto]=master
-hashes[hacl-star]=5e7b53beb6b316a703b1b0ae723dec02e1803b91
+hashes[hacl-star]=f992fa8f811059c6f68d4886f0b726962afb88d9
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=8639e19b6306db085552dd38b3ea14044b7a4d10
+hashes[kremlin]=33d068eaa4d6be431c3927097af096e66d8c9053
 branches[kremlin]=fstar-master
