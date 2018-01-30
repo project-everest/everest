e3ff10c835191ba7c74b1cd0220dd70e81f038ba
Nikhil Swamy <nikswamy@users.noreply.github.com>
2018-01-30 15:22:40 -0800

    snap

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=c3bd51933148f0672e91663d291705375f771769
+hashes[mitls-fstar]=07c54bea45c3421af843974f3c0e8fa687c1fc7e
 branches[mitls-fstar]=quic2c
 hashes[vale]=fa2e31b8f44debcc43a1d8ec0f6fdeecb2ce6782
 branches[vale]=master
-hashes[FStar]=e287c00e95afa305d101ec4c115944e5f14c0dfb
+hashes[FStar]=ff60a6f968df9de6ae16460a411bcddcb849c791
 branches[FStar]=master
 hashes[MLCrypto]=29ecaa33e3c3cafb81c737d1acfd5eff8b278bd3
 branches[MLCrypto]=master
-hashes[hacl-star]=c40fe0aaeb00ebd454a4b69d4c28b9e2ad8ec980
+hashes[hacl-star]=7c37c4d1570ffd2a886fd9fccea7f0abed0f44e2
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=54a53c46bda76ef4c8e88ef90eff885c61854657
+hashes[kremlin]=68bfd3190dea3448a548917deae6674c7257242e
 branches[kremlin]=fstar-master
