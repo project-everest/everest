59c0b0a49fd290860309ea67d6acd663abeffa51
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-01-30 14:12:20 -0800

    Try again with Makefile bugs fixed

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=0b88be039b48d301748151493c02b53ae4a84286
+hashes[mitls-fstar]=bff0cc3e03e728646203f1e0b6a68bcfac331bf4
 branches[mitls-fstar]=quic2c
 hashes[vale]=fa2e31b8f44debcc43a1d8ec0f6fdeecb2ce6782
 branches[vale]=master
@@ -8,7 +8,7 @@ hashes[FStar]=e287c00e95afa305d101ec4c115944e5f14c0dfb
 branches[FStar]=master
 hashes[MLCrypto]=29ecaa33e3c3cafb81c737d1acfd5eff8b278bd3
 branches[MLCrypto]=master
-hashes[hacl-star]=7c37c4d1570ffd2a886fd9fccea7f0abed0f44e2
+hashes[hacl-star]=c40fe0aaeb00ebd454a4b69d4c28b9e2ad8ec980
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=e3ab3373f19135566db0c0a65ae4aace9bed5b69
+hashes[kremlin]=54a53c46bda76ef4c8e88ef90eff885c61854657
 branches[kremlin]=fstar-master
