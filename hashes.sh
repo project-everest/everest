26880579ea6fce80b12d5a3cd5685be87c0fb658
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-01-31 09:23:44 -0800

    How about this new build of HACL*

--- hashes.sh
+++ hashes.sh
@@ -2,13 +2,13 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=6a43af55f031f552d08ed17ef7f49f263d28f35c
 branches[mitls-fstar]=quic2c
-hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
+hashes[vale]=fa2e31b8f44debcc43a1d8ec0f6fdeecb2ce6782
 branches[vale]=master
 hashes[FStar]=ff60a6f968df9de6ae16460a411bcddcb849c791
 branches[FStar]=master
 hashes[MLCrypto]=29ecaa33e3c3cafb81c737d1acfd5eff8b278bd3
 branches[MLCrypto]=master
-hashes[hacl-star]=b95558cc3d49747730e18a4713fbeab8cbb0a053
+hashes[hacl-star]=aa6712a0ebe8a8611825b9716fecbfd2079063fc
 branches[hacl-star]=c_mitls2c
 hashes[kremlin]=68bfd3190dea3448a548917deae6674c7257242e
 branches[kremlin]=fstar-master
