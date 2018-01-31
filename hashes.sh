24fcbc5686f249a9d35eda9aa0b194fc36518767
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-01-31 13:30:26 -0800

    Try new snapshot

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=f2877f13ed070914a5dd5a95051dd1af8a8878c7
+hashes[mitls-fstar]=191d2c722ba29115d6ad4095ee31b4a58f9bee31
 branches[mitls-fstar]=quic2c
 hashes[vale]=fa2e31b8f44debcc43a1d8ec0f6fdeecb2ce6782
 branches[vale]=master
-hashes[FStar]=ff60a6f968df9de6ae16460a411bcddcb849c791
+hashes[FStar]=dd7e06135fd18816559999071c4e75988df13f43
 branches[FStar]=master
 hashes[MLCrypto]=29ecaa33e3c3cafb81c737d1acfd5eff8b278bd3
 branches[MLCrypto]=master
 hashes[hacl-star]=aa6712a0ebe8a8611825b9716fecbfd2079063fc
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=68bfd3190dea3448a548917deae6674c7257242e
+hashes[kremlin]=4822e0e3644fabb52596e1a8847d78ed124e7fd5
 branches[kremlin]=fstar-master
