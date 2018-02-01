9f57f0926c79e782fc8d4916de088cd1d7a82ea4
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-02-01 13:29:58 -0800

    Check that the build is not broken

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=46d96585f61d3d73865c7754b08f08ac5c16028e
+hashes[mitls-fstar]=9534853be3c73998ca069063deeff313b46b510c
 branches[mitls-fstar]=quic2c
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
@@ -8,7 +8,7 @@ hashes[FStar]=1fbbb4d6e47ff12a34a12c37aa33058ff5e839a6
 branches[FStar]=master
 hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
-hashes[hacl-star]=f8ebc6a3cf6fffcada9f59fc4096e28dcdad7b48
+hashes[hacl-star]=960f4a4782994ee96ba6377f95b73817a86b9403
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=d3b4a9b78ad0e7fa29520731dfa4149f4ef68350
+hashes[kremlin]=a67a98788572013b44934582bd5904fca09bd367
 branches[kremlin]=fstar-master
