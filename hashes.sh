fb3f0750299db1ccf2a5c92ecb90b54bb8b40c6a
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-02-01 13:13:45 -0800

    Try UInt128

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=46d96585f61d3d73865c7754b08f08ac5c16028e
+hashes[mitls-fstar]=b7ecc79bd7b382d5d15c9a378d118bfafe535665
 branches[mitls-fstar]=quic2c
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
@@ -8,7 +8,7 @@ hashes[FStar]=1fbbb4d6e47ff12a34a12c37aa33058ff5e839a6
 branches[FStar]=master
 hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
-hashes[hacl-star]=f8ebc6a3cf6fffcada9f59fc4096e28dcdad7b48
+hashes[hacl-star]=5c704334a5b482028809b46f05cfa33fc00d5097
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=d3b4a9b78ad0e7fa29520731dfa4149f4ef68350
+hashes[kremlin]=53c3774bcfb0f7bb8419f29640041c985e1359f8
 branches[kremlin]=fstar-master
