0a84299ecb9a7d54ebad45c6c9b0352c017f9709
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-02-02 10:42:14 -0800

    Hashes that worked for me

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=3b6f4d4609310cb83afb89bcccc4998588dce6a0
+hashes[mitls-fstar]=1323d240de6084e511d49a93a7468a892f0bdcb7
 branches[mitls-fstar]=quic2c
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
-hashes[FStar]=bf6e23954e078c8b90a9085deb2ee908d062624d
+hashes[FStar]=1fbbb4d6e47ff12a34a12c37aa33058ff5e839a6
 branches[FStar]=master
 hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
-hashes[hacl-star]=76cbc9fd489224e65030bc64fa187086703b5163
+hashes[hacl-star]=601b9911e2fb34df308ba981b78be499734789e1
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=590967f8a7a1b3568cdfa460f5f9c6358f6b813a
+hashes[kremlin]=2a69ab56850cb50d691929a925ce47af60c213f0
 branches[kremlin]=fstar-master
