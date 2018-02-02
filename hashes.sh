101c9bf9d0283f1d06b56d2f048ea5dcb7caa808
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-02-01 16:38:56 -0800

    This gave me a green

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=9534853be3c73998ca069063deeff313b46b510c
+hashes[mitls-fstar]=53ecbdf574d96183bf2676230b570dc5492a08b6
 branches[mitls-fstar]=quic2c
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
@@ -8,7 +8,7 @@ hashes[FStar]=1fbbb4d6e47ff12a34a12c37aa33058ff5e839a6
 branches[FStar]=master
 hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
-hashes[hacl-star]=960f4a4782994ee96ba6377f95b73817a86b9403
+hashes[hacl-star]=336cdb9691954c8e6e9089c742cbed9e16cfd4a0
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=024c6a08dc31607c4bdce6cbb8d42ff6a1a9b1ea
+hashes[kremlin]=c9d3ebaf2426d6fc2e79673c40a2ce559795087c
 branches[kremlin]=fstar-master
