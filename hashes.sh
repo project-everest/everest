d16125c6e315cc464391f9bc8ac306fa5bab475d
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-02-07 14:15:11 -0800

    Try new hashes

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=163dee4efee86b71894c5777d2aa73c017026b3f
+hashes[mitls-fstar]=143c40e4785427422c91f6184dc674b3be160993
 branches[mitls-fstar]=quic2c
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
-hashes[FStar]=90d259daa06351dd853d501a33924c8756fa89d4
+hashes[FStar]=5ef299eba189096dd5c9e6271c779e726476e951
 branches[FStar]=master
 hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
-hashes[hacl-star]=1df3114feb599dde3faf4ad929cc78ee5c9534bb
+hashes[hacl-star]=7679433dac20ce9abfc764cd3e365e117148ea84
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=b93c5ed83cda0395449c0e5611680d447eaa4bf9
+hashes[kremlin]=fd656e3ba1e40104fb5163ab402ca099b87611c6
 branches[kremlin]=fstar-master
