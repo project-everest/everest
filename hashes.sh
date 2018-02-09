e6e1567718a40b8ebc5fb03aa1c694a467339754
Tahina Ramananandro <taramana@microsoft.com>
2018-02-08 16:25:08 -0800

    With extraction now

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=7f288335b14470c6e3a670868536afc7db791378
-branches[mitls-fstar]=quic2c
+hashes[mitls-fstar]=705d54dc363f6172da35ea08fb4b7c197f1d9cad
+branches[mitls-fstar]=taramana_quic2c_parsers_success_20180208
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
-hashes[FStar]=90d259daa06351dd853d501a33924c8756fa89d4
+hashes[FStar]=59567133310f87f4fe76f7c433f2fe6d8112da57
 branches[FStar]=master
 hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
 hashes[hacl-star]=1df3114feb599dde3faf4ad929cc78ee5c9534bb
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=439687d9a1ef3dd2063f9d5605b628b897dce65b
+hashes[kremlin]=4bc1f7a2d52a2b9546c40d6f904eca233d62a824
 branches[kremlin]=fstar-master
