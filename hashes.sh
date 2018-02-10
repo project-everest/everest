8c7f4c47307505bd517cdc0ac9e476401adbf8fd
Tahina Ramananandro <taramana@microsoft.com>
2018-02-09 16:14:49 -0800

    Linking: test new version scripts

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=949c7c9b51c3f349453fd8c9c4477faf67bc0f7c
+hashes[mitls-fstar]=4d402e32ff5940bc03ee916afb37777664b03033
 branches[mitls-fstar]=quic2c
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
-hashes[FStar]=5ef299eba189096dd5c9e6271c779e726476e951
+hashes[FStar]=de92c9cce1f76b80e3f6de77aac6786b9d4c12ee
 branches[FStar]=master
 hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
-hashes[hacl-star]=7679433dac20ce9abfc764cd3e365e117148ea84
+hashes[hacl-star]=c2c70d0f1c805ef0144fe48124e4333390f3c694
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=fd656e3ba1e40104fb5163ab402ca099b87611c6
+hashes[kremlin]=4bc1f7a2d52a2b9546c40d6f904eca233d62a824
 branches[kremlin]=fstar-master
