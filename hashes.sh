8669e7013cbfc77486adec975a2ce544455a2d3d
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-02-15 10:57:20 -0800

    New hashes

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=1be46cc2424d8c4503e41cd457351232ab826572
+hashes[mitls-fstar]=763df162bb5e9c14262a8afecbbfbcb8ca903437
 branches[mitls-fstar]=quic2c
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
-hashes[FStar]=738c824f9ddad1bee76c8f283a8d40dead1477a8
+hashes[FStar]=83cd627cb987800196e85e26eae3de9d434a3af5
 branches[FStar]=master
 hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
-hashes[hacl-star]=01a3d1ddd14d958a0bd5fcfae76dc5c8b5183fb1
+hashes[hacl-star]=790f804db306efa5528459ab3ac2aa0a0b40e2ce
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=dfcc9bedc03e3b923f49598828361c35cee9eed9
+hashes[kremlin]=bd3209dbcc6afe2db774ee577ef07bc30182abbe
 branches[kremlin]=fstar-master
