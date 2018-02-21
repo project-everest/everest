5fb890d9b7645b14358e7fb6d7c18d1f5540680b
Tahina Ramananandro <taramana@microsoft.com>
2018-02-21 13:09:12 -0800

    Update
    ***NO_CI***

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=4eaf96056bff1a72135c8c2bbcd7d839e33eae42
+hashes[mitls-fstar]=ce150c18f09ac468083420496bfbbbcad5bcb945
 branches[mitls-fstar]=quic2c_parsers
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
-hashes[FStar]=aa94aad99e3ec4fe330b150a0784ca0a1357a040
+hashes[FStar]=bb2c63aacfd10dcdb269228640860dd57eea995f
 branches[FStar]=master
 hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
 hashes[hacl-star]=01a3d1ddd14d958a0bd5fcfae76dc5c8b5183fb1
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=dfcc9bedc03e3b923f49598828361c35cee9eed9
+hashes[kremlin]=51e8dc306e701507cece7c86d4b7792acca3734d
 branches[kremlin]=fstar-master
