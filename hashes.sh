4cd1c84edf55109e597ea99e6de6c99fdc920769
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-02-23 14:02:10 -0800

    Newer Kremlin

--- hashes.sh
+++ hashes.sh
@@ -10,5 +10,5 @@ hashes[MLCrypto]=806647fcdc7f1da1db38191e3520988e7500331a
 branches[MLCrypto]=master
 hashes[hacl-star]=a546acb8027319df7df8d957be5a3bcbe8ba5646
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=187f4f7e11cd0ab31a01d72476b16a9f79836d7e
+hashes[kremlin]=1696b3ab1d7feaa201bf79d7ebb745159f54e8fe
 branches[kremlin]=fstar-master
