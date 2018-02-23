856b547d16069cb4325ab97bab110f3ed9aee942
s-zanella <santiago@microsoft.com>
2018-02-23 11:09:10 +0000

    Add QUIC repository

--- repositories.sh
+++ repositories.sh
@@ -1,9 +1,22 @@
 declare -A repositories
 
-repositories[FStar]=git@github.com:FStarLang/FStar.git
-repositories[kremlin]=git@github.com:FStarLang/kremlin.git
-repositories[hacl-star]=git@github.com:mitls/hacl-star.git
-repositories[mitls-fstar]=git@github.com:mitls/mitls-fstar.git
-repositories[vale]=git@github.com:project-everest/vale.git
-repositories[MLCrypto]=git@github.com:project-everest/MLCrypto.git
 repositories[QUIC]=msresearch@vs-ssh.visualstudio.com:22/DefaultCollection/Everest/_ssh/QUIC
+https[QUIC]=https://msresearch.visualstudio.com/DefaultCollection/Everest/_git/QUIC
+
+repositories[FStar]=git@github.com:FStarLang/FStar.git
+https[FStar]=https://github.com/FStarLang/FStar.git
+
+repositories[kremlin]=git@github.com:FStarLang/kremlin.git
+https[kremlin]=https://github.com/FStarLang/kremlin.git
+
+repositories[hacl-star]=git@github.com:mitls/hacl-star.git
+https[hacl-star]=https://github.com/mitls/hacl-star.git
+
+repositories[mitls-fstar]=git@github.com:mitls/mitls-fstar.git
+https[mitls-fstar]=https://github.com/mitls/mitls-fstar.git
+
+repositories[vale]=git@github.com:project-everest/vale.git
+https[vale]=https://github.com/project-everest/vale.git
+
+repositories[MLCrypto]=git@github.com:project-everest/MLCrypto.git
+https[MLCrypto]=https://github.com/project-everest/MLCrypto.git
