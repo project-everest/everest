44b2685ec6bc27fc2e52e16b42486917523877ac
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-03-09 14:10:09 -0800

    This builds the internal test with -fnostruct-passing

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=18ebf8180285065914f44d2546669c480f4eaf46
-branches[mitls-fstar]=quic2c
+hashes[mitls-fstar]=00620bd02ba2f58ad7f485ce2b2446baf9980b0b
+branches[mitls-fstar]=protz_nostruct_passing
 hashes[vale]=e7443d77a0c5953d591e24b807146ef2efb3e557
 branches[vale]=master
-hashes[FStar]=0f60ee4e50529c44ef4c495e73e6366e524f5bbe
+hashes[FStar]=9ddb3a0b7b86a86a4324feee8e8a33b698a2b164
 branches[FStar]=master
 hashes[MLCrypto]=ca85e7a6b83b4eddb85533dfd265395fbb76971e
 branches[MLCrypto]=master
-hashes[hacl-star]=ecae6db670bff33110108264dca7bacb5462c114
-branches[hacl-star]=c_mitls2c
-hashes[kremlin]=4a130ec39db03271cf448577f880da77f887646c
+hashes[hacl-star]=492973e7cf3e9b0c13a36aa776d984d1deae0516
+branches[hacl-star]=protz_nostruct_passing
+hashes[kremlin]=250fa58b95637c833954eed9f2ffb5cf8adc9f75
 branches[kremlin]=fstar-master
