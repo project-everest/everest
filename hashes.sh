3bca0fab0824419558ae0c282e3225b11f7f8ac3
s-zanella <santiago@microsoft.com>
2018-03-13 11:47:28 +0000

    Update

--- hashes.sh
+++ hashes.sh
@@ -1,10 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=e5067edc0ab83489bc8de02ece8c9d8bd4735882
+hashes[mitls-fstar]=1130722db6fec499403a5b3848d018cbee6db8c4
 branches[mitls-fstar]=quic2c
 hashes[vale]=e7443d77a0c5953d591e24b807146ef2efb3e557
 branches[vale]=master
-hashes[FStar]=682dc4d2e621bab000e7827d828eff156246da76
+hashes[FStar]=673ffabc240bbe54db6564861b7c4f207e335192
 branches[FStar]=master
 hashes[MLCrypto]=ca85e7a6b83b4eddb85533dfd265395fbb76971e
 branches[MLCrypto]=master
