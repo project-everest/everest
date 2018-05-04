87fdd9b8f332611ce9d0e145d12e6ca60c0efcd6
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-05-04 13:19:46 -0400

    Try again

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=747438d41642e1e6d36908e30f0254e89ad79711
+hashes[mitls-fstar]=573f15c92266471775a3c7ade5af6115cfb155fb
 branches[mitls-fstar]=cleanup-kremlib-2
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
@@ -10,5 +10,5 @@ hashes[MLCrypto]=12f3f1e1eae7e78f1260b04794c481814f6d12a6
 branches[MLCrypto]=master
 hashes[hacl-star]=73578e1a69c70f263a607d23676b76acd8437072
 branches[hacl-star]=cleanup-kremlib-2
-hashes[kremlin]=dc17838a2d79fc264cb05dea4aeedf8480cf9b87
+hashes[kremlin]=677f89d68e7c9aae019997dc7f595c3ad22c0f31
 branches[kremlin]=cleanup-kremlib-2
