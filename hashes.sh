a8bb462b12681dba054fb61ba545969c4a71a88d
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-05-07 12:26:37 +0200

    Merged

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=d77db026ded614f854948dd8a62ae246905e8668
-branches[mitls-fstar]=cleanup-kremlib-2
+hashes[mitls-fstar]=773845a4e4519f5f8f5171d9aaf6e94c6bc9d227
+branches[mitls-fstar]=quic2c
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
 hashes[FStar]=ca297a094b93ac1fd797ce82b6f6abbfefe85036
 branches[FStar]=master
 hashes[MLCrypto]=12f3f1e1eae7e78f1260b04794c481814f6d12a6
 branches[MLCrypto]=master
-hashes[hacl-star]=b2b8eecaa1e325d09b04b9c53c52abb0f6bd2e0a
-branches[hacl-star]=cleanup-kremlib-2
+hashes[hacl-star]=bc3e49ce61d7731d7e15feda1b8e3daf5d2f06dc
+branches[hacl-star]=fstar-master
 hashes[kremlin]=2739d4f8fead6bf5282c06f94dfb9f9faf8c825f
-branches[kremlin]=cleanup-kremlib-2
+branches[kremlin]=fstar-master
