32d68d5bf2718b8e159376d68961cfe6441612fb
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-05-07 14:38:02 +0200

    Renaminsg

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=773845a4e4519f5f8f5171d9aaf6e94c6bc9d227
+hashes[mitls-fstar]=c2c76997f04f914363d39aca546efa988ca21477
 branches[mitls-fstar]=quic2c
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
@@ -10,5 +10,5 @@ hashes[MLCrypto]=12f3f1e1eae7e78f1260b04794c481814f6d12a6
 branches[MLCrypto]=master
 hashes[hacl-star]=bc3e49ce61d7731d7e15feda1b8e3daf5d2f06dc
 branches[hacl-star]=fstar-master
-hashes[kremlin]=2739d4f8fead6bf5282c06f94dfb9f9faf8c825f
+hashes[kremlin]=b254937e84b0eb3f8a96b42839b69f8141be9bf3
 branches[kremlin]=fstar-master
