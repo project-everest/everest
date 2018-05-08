ba792062cc1e181659ec53f59abec86b3d7825e0
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-05-09 00:24:35 +0200

    More uint128 woes

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=09fc057cccb3396780905da21d7c930d176281c1
+hashes[mitls-fstar]=afc1b62ca85b45b89dc47e5c4c6d19535f4c947d
 branches[mitls-fstar]=quic2c
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
@@ -8,7 +8,7 @@ hashes[FStar]=7532fd1de11c33b1a627da629ed52349ffca8479
 branches[FStar]=master
 hashes[MLCrypto]=12f3f1e1eae7e78f1260b04794c481814f6d12a6
 branches[MLCrypto]=master
-hashes[hacl-star]=09f161a4887d056b54d5054a0b5c5027e7a40df4
+hashes[hacl-star]=effc053b69f98df8f808e65b68b08a89c9aac2d7
 branches[hacl-star]=fstar-master
-hashes[kremlin]=b254937e84b0eb3f8a96b42839b69f8141be9bf3
+hashes[kremlin]=9daaf1f8d8b63f82f6a1d3ec9013f6f20d7e0127
 branches[kremlin]=fstar-master
