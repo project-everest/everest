f53e3515858603fce0a3a16076c7cfdb23ebb674
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-05-08 15:12:17 +0200

    Try some CI

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=84f2a992d0b3e860d3b0fbce2819062941107302
-branches[mitls-fstar]=quic2c
+hashes[mitls-fstar]=e77d5725806a1be781d179eb23f29cb1391c8651
+branches[mitls-fstar]=evercrypt
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
 hashes[FStar]=7532fd1de11c33b1a627da629ed52349ffca8479
 branches[FStar]=master
 hashes[MLCrypto]=12f3f1e1eae7e78f1260b04794c481814f6d12a6
 branches[MLCrypto]=master
-hashes[hacl-star]=347be92db994d95c1c4bf784f442b433a9c46d3b
+hashes[hacl-star]=dc2ec281e23ae9bb5fba7bc8e9cfd1041dfedcb5
 branches[hacl-star]=fstar-master
 hashes[kremlin]=b254937e84b0eb3f8a96b42839b69f8141be9bf3
 branches[kremlin]=fstar-master
