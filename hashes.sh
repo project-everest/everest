39fb18c004467f25eb5039752b78fb006e397179
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-05-09 11:17:32 +0200

    Fresh try for the provider

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=fd04ee350502be7c61a055a642c12f507f5d37fa
+hashes[mitls-fstar]=619f0e00d034c1ba420eba8cb19f1ce4a8548cb0
 branches[mitls-fstar]=evercrypt
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
@@ -8,7 +8,7 @@ hashes[FStar]=7532fd1de11c33b1a627da629ed52349ffca8479
 branches[FStar]=master
 hashes[MLCrypto]=12f3f1e1eae7e78f1260b04794c481814f6d12a6
 branches[MLCrypto]=master
-hashes[hacl-star]=dc2ec281e23ae9bb5fba7bc8e9cfd1041dfedcb5
+hashes[hacl-star]=effc053b69f98df8f808e65b68b08a89c9aac2d7
 branches[hacl-star]=fstar-master
-hashes[kremlin]=b254937e84b0eb3f8a96b42839b69f8141be9bf3
+hashes[kremlin]=9daaf1f8d8b63f82f6a1d3ec9013f6f20d7e0127
 branches[kremlin]=fstar-master
