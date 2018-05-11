a86d98e37ad1178fe93c20910c29d314154f5d17
Nikhil Swamy <nikswamy@users.noreply.github.com>
2018-05-11 10:03:45 -0700

    in progress merge

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=93482a1219310c705c8e186f978b9e1699176b97
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=908872e6427d53cf0fac89de85633fa716541288
+branches[mitls-fstar]=merge_fstar_master_20180510
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
-hashes[FStar]=ca297a094b93ac1fd797ce82b6f6abbfefe85036
+hashes[FStar]=9bfe0a6142e70d1646af351175aad1bdbfb85ee8
 branches[FStar]=master
 hashes[MLCrypto]=12f3f1e1eae7e78f1260b04794c481814f6d12a6
 branches[MLCrypto]=master
-hashes[hacl-star]=70f9797da4ae2fe1331ee691becd0549617e37cb
-branches[hacl-star]=fstar-master
+hashes[hacl-star]=52003cee574cd042b14a86401f3b622f3050b52c
+branches[hacl-star]=merge_fstar_master_20180510
 hashes[kremlin]=a3d914df14ee1fe9b7709955a8e78acf07928f0d
 branches[kremlin]=fstar-master
