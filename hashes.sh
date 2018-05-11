11ea7be96778ac10ac1d0bcb97f30b3f7f547396
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-05-11 13:59:14 +0200

    Try with a slightly older version of F*

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=619f0e00d034c1ba420eba8cb19f1ce4a8548cb0
+hashes[mitls-fstar]=a1a11c4010402fdc34e372759e510c4fb3ccb614
 branches[mitls-fstar]=evercrypt
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
@@ -8,7 +8,7 @@ hashes[FStar]=473d2dc139435c9596f98f396eeebead414a8456
 branches[FStar]=master
 hashes[MLCrypto]=12f3f1e1eae7e78f1260b04794c481814f6d12a6
 branches[MLCrypto]=master
-hashes[hacl-star]=d729bf57852a9662d8a6051bda9fcfbb3ef72439
+hashes[hacl-star]=d037b66feb4abfceb876590f3685c271bfbc9eb6
 branches[hacl-star]=fstar-master
-hashes[kremlin]=ce5080e519c6d8d52c852646264ef54e1e471d0e
+hashes[kremlin]=879b0696bbb902bf04085b35f13f697b982b1b4d
 branches[kremlin]=fstar-master
