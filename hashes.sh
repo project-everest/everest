6f75e6015786daeeaa57312a492da077e26072a6
s-zanella <santiago@microsoft.com>
2018-05-16 14:44:16 +0100

    Update after merging quic2c and evercrypt

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=37bf8903f2d4b4541d9556d5c6b3f25d8ea09908
+hashes[mitls-fstar]=a3b0409400ba4f6e8cb3cf877fa0528a8ff9b876
 branches[mitls-fstar]=merge_fstar_master_20180510
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
@@ -8,7 +8,7 @@ hashes[FStar]=81cafe5d334597fdae41c985bc2570a4f49f4dad
 branches[FStar]=master
 hashes[MLCrypto]=6a1239245898101aea984d18e239160c78374941
 branches[MLCrypto]=master
-hashes[hacl-star]=1e498f6ff869034b37d4efea57f165bbde2f7d67
+hashes[hacl-star]=e515d44c99d3f84e508ac2db87a96b8fefaf0937
 branches[hacl-star]=merge_fstar_master_20180510
 hashes[kremlin]=e28a1bdab6d11b1d540e57bd1f97bd88e3f84335
 branches[kremlin]=fstar-master
