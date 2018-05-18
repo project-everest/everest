ca0a1c92e95eed154547d624b97170911c5dc112
s-zanella <santiago@microsoft.com>
2018-05-18 12:26:05 +0100

    Update

--- hashes.sh
+++ hashes.sh
@@ -1,10 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=3e1eaf778105c7f8ad661dbae8182617d564b2c3
+hashes[mitls-fstar]=acec7131bbf58d5dc80b99fe9451cbd2e329c28c
 branches[mitls-fstar]=merge_fstar_master_20180510
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
-hashes[FStar]=a0a6742d4dd657bd084c5a19ee53f7c75f03acab
+hashes[FStar]=f464bd99a01b202c16531d12547201deea5af4b3
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
