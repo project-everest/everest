346481552df5b04449dfaf67d28a7f4ee81d953d
s-zanella <santiago@microsoft.com>
2018-05-24 14:56:13 +0100

    Update; hopefully fixin qbuild

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=acec7131bbf58d5dc80b99fe9451cbd2e329c28c
+hashes[mitls-fstar]=a54fd559c01273244854efb2cb5d9da3aa48c573
 branches[mitls-fstar]=master
 hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
 branches[vale]=master
-hashes[FStar]=f464bd99a01b202c16531d12547201deea5af4b3
+hashes[FStar]=c860bb2b37177639ac92474dc2aa6e4d4a3b0b7a
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=a3d9c67d2de879f8c964c63128a4fd077d8b87bb
+hashes[hacl-star]=16c4ca68cc26628d967e641c110ccd6f264b32e5
 branches[hacl-star]=fstar-master
-hashes[kremlin]=d5538f7d957497195e4df93147df518c5224cda9
+hashes[kremlin]=6c3b087f150286f6919bb6865bf5b990e97d7a01
 branches[kremlin]=master
