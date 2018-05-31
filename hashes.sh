830718d31d40701884ccbc32745896c8a82a9036
Santiago Zanella-Beguelin <santiago@microsoft.com>
2018-05-31 17:27:11 +0100

    Update

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=286abafa4d984e68b6454869059b98015ddd45a1
+hashes[mitls-fstar]=c578319ee8b65bf4402724721e9880a10355a74e
 branches[mitls-fstar]=master
-hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
+hashes[vale]=83c53a281cbd02fbb75a7ebeff875281dbb783d2
 branches[vale]=master
-hashes[FStar]=f4dc42544852a2c523dd154824a6996e2700279d
+hashes[FStar]=819274735e2a6999b9282d1454210c917065ceec
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
 hashes[hacl-star]=d669ec0560b4091b2101c8efad2983b14fafdcda
 branches[hacl-star]=fstar-master
-hashes[kremlin]=8f5214024dbcb63c8b8980386f8d49a19c3fd530
+hashes[kremlin]=fdc1bd899e717dbfbad2c039bec156c720b394bd
 branches[kremlin]=master
