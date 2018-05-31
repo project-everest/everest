00e48d4e658be3a461c33b0a2f0a466bceb399ca
Antoine Delignat-Lavaud <antdl@microsoft.com>
2018-05-31 16:45:09 +0100

    Merge make-source-drop from master

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=2df88f3ff47a0447ca1cd65396e1eaab4ef3841b
+hashes[mitls-fstar]=8820c20b39f5462d9b1b3b7ac7e7bc663ba6fdb3
 branches[mitls-fstar]=quic2c
-hashes[vale]=5f0c2bb91599bdde3727126b6d306eedb82ce472
-branches[vale]=master
-hashes[FStar]=97259ecaed3801659e8c13c27027fc2c2117e0bb
+hashes[vale]=be313e14cef1ddfee8ce6827fdc5e70951b7e497
+branches[vale]=fstar
+hashes[FStar]=dba0628c3975234bd41380f1b9d933162a17eae1
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
 hashes[hacl-star]=94776d1768b9f5817936abf71fe77235592e534e
 branches[hacl-star]=fstar-master
-hashes[kremlin]=3138c12b1d40efdb6257c9f48c388d6334cab078
+hashes[kremlin]=fdc1bd899e717dbfbad2c039bec156c720b394bd
 branches[kremlin]=master
