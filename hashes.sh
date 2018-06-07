3ea4227f7ba2a05e944dadab5f7b3f0b1983339f
Jonathan Protzenko <protz@microsoft.com>
2018-06-07 15:43:09 -0700

    These work for me on windows

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=6690ac8d514171583f25c376b44a2e72663be652
+hashes[mitls-fstar]=98eb579de379d6c0693b3bc6b76ddc43b512c51e
 branches[mitls-fstar]=master
 hashes[vale]=f678c15721eb4d7ab60676b3193a64f149d01a48
 branches[vale]=master
-hashes[FStar]=99b6688fe484394a8d714cce4bd694d02a034fb1
+hashes[FStar]=86c688950b83490b202c77f013d394615f378791
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=ccc0bc598320c8f3fecd18f425c387bd06511ffd
+hashes[hacl-star]=de1358a689fe10b1dd2611e3bdee74494d75fe99
 branches[hacl-star]=fstar-master
-hashes[kremlin]=fc9191bfd0dfdd8a6978a773a98e5b072b117887
+hashes[kremlin]=d2a0d29b65d862ffad79ab512a74926e0c6b8229
 branches[kremlin]=master
