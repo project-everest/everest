1b64976b287a0201863ea5ca42932dda3413e676
Jonathan Protzenko <protz@microsoft.com>
2018-06-11 19:02:59 -0700

    green?

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=3a031283ab618a059627f0b746b885a774e1582d
+hashes[mitls-fstar]=d6f1e26eabfc221c39143303e77a8a26b901441b
 branches[mitls-fstar]=master
 hashes[vale]=223755445e0fbdc2eef274f38549738c43ec0472
 branches[vale]=fstar
@@ -8,7 +8,7 @@ hashes[FStar]=86c688950b83490b202c77f013d394615f378791
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=2a68b1421ded0f9e034c5b67b59ff5036da78444
+hashes[hacl-star]=acea865a4ed72f54988979a93b5807d6500b861b
 branches[hacl-star]=fstar-master
 hashes[kremlin]=d2a0d29b65d862ffad79ab512a74926e0c6b8229
 branches[kremlin]=master
