06ea4802bbb2e541d883a27191064b0907392084
Jonathan Protzenko <protz@microsoft.com>
2018-06-19 07:20:40 -0700

    drop improvements, linking goes further...

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=18af0425412e724bbfa460d0b1e214ef8caeee7a
+hashes[mitls-fstar]=4f18ca1d1eb5db3d2111252a50757ea765e29fdb
 branches[mitls-fstar]=protz_uint128
 hashes[vale]=f678c15721eb4d7ab60676b3193a64f149d01a48
 branches[vale]=master
@@ -8,7 +8,7 @@ hashes[FStar]=31ad7bbbec048f8d131b851486342eb0d816f262
 branches[FStar]=protz_uint128
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=0fb00252fcc7dd2a2395e8db7dbd66f32ff5d512
+hashes[hacl-star]=92343f04757e1be53dbe85e304ed6f23f39f4635
 branches[hacl-star]=protz_uint128
-hashes[kremlin]=2598a7cee555005c03bf2d994661b56da467d77f
+hashes[kremlin]=aaff35254754ff517bb72eaea4a69cafb981c3dc
 branches[kremlin]=protz_uint128
