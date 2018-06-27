3f677cf58e2cdb69964ca85ed9d02a6205494664
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-06-27 08:50:39 -0700

    Merge

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=e578b0c2d1bca7283b890f76691200dcb8db101f
+hashes[mitls-fstar]=86998ad456b70b2e708df98bc916369a2d5f33c2
 branches[mitls-fstar]=protz_uint128
-hashes[vale]=f678c15721eb4d7ab60676b3193a64f149d01a48
+hashes[vale]=10b3f6c9d98db1317ffe65b2b0078c44e626c2e6
 branches[vale]=master
-hashes[FStar]=31ad7bbbec048f8d131b851486342eb0d816f262
+hashes[FStar]=65a0698e103ca24de98e69ab56562720a5e3f9df
 branches[FStar]=protz_uint128
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=92343f04757e1be53dbe85e304ed6f23f39f4635
+hashes[hacl-star]=4200e7e289b7fff785c69bacf610c7e0371baa1f
 branches[hacl-star]=protz_uint128
-hashes[kremlin]=aaff35254754ff517bb72eaea4a69cafb981c3dc
+hashes[kremlin]=8342252aed9b9c71d912579b247c9db199aa6687
 branches[kremlin]=protz_uint128
