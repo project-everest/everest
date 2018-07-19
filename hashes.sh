729d0959fd93ff5eab5e23dcad03032b8980f959
Antoine Delignat-Lavaud <antdl@microsoft.com>
2018-07-19 19:40:58 +0100

    Upgrade

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=9a1d0a10e223df76f5ec39c49ddae27e87fe27a2
+hashes[mitls-fstar]=e2121baf16068f9d5dc8b5744d41641449a481d3
 branches[mitls-fstar]=adl_quic12
 hashes[vale]=f0903cd82ef556d57d72305bdee7d6c4c4ec66f0
 branches[vale]=aes_bytes
-hashes[FStar]=2959985051f1d6c9975e3a8051f6209fa8d94614
+hashes[FStar]=8d68de3a6d6b8d1b53766a9f0959370fe23b6957
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=ad6fcfe03e4e114becc08157ce95516145f5427b
+hashes[hacl-star]=11a2c8cf3a41cdbe6c5ddcdc858fba345dd5ffdd
 branches[hacl-star]=fstar-master
-hashes[kremlin]=4707ffae45dd137ccf0fde7c749f244e460abb6e
+hashes[kremlin]=4582b574be965bbfd0f67b72bd3499396fbe8160
 branches[kremlin]=master
