b8250097759568ae5a2ee17a680175a96dbb783a
Antoine Delignat-Lavaud <antdl@microsoft.com>
2018-07-20 17:40:56 +0100

    New hashes

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=e2121baf16068f9d5dc8b5744d41641449a481d3
+hashes[mitls-fstar]=ab2ce4e64e6fb3e806ad380dbae5f91a13375695
 branches[mitls-fstar]=adl_quic12
 hashes[vale]=f0903cd82ef556d57d72305bdee7d6c4c4ec66f0
 branches[vale]=aes_bytes
-hashes[FStar]=8d68de3a6d6b8d1b53766a9f0959370fe23b6957
+hashes[FStar]=57708bdbd6d8b46313409139bc83e6a3a209e8d8
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=11a2c8cf3a41cdbe6c5ddcdc858fba345dd5ffdd
+hashes[hacl-star]=6729c8b9eca2c84aac907a7b2c3d147e4f244885
 branches[hacl-star]=fstar-master
 hashes[kremlin]=4582b574be965bbfd0f67b72bd3499396fbe8160
 branches[kremlin]=master
