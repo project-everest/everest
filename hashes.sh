868d3b803f277a0308dcb810561bef0b00990ffb
Antoine Delignat-Lavaud <antdl@microsoft.com>
2018-08-06 14:04:58 +0100

    Clean up source drop

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=a10c7c8662fdb5088919042fd21734d69238bbfe
+hashes[mitls-fstar]=17af1612035fe7b71631f1fcbbf22579200905de
 branches[mitls-fstar]=master
 hashes[vale]=f0903cd82ef556d57d72305bdee7d6c4c4ec66f0
 branches[vale]=aes_bytes
-hashes[FStar]=a5e410c943593278809af8c9493ffbcff4d53dcd
+hashes[FStar]=598b3dcb2ac9b88e11d811a5756b82f5445e9104
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=7f313ae14170fafc664c6c8c92c7d6d071ba8414
+hashes[hacl-star]=0a5f4cfc2f3c7d193547cad54c939bab41c108b9
 branches[hacl-star]=fstar-master
 hashes[kremlin]=3de22d33d7a04daab497573b6f4220156136c366
 branches[kremlin]=master
