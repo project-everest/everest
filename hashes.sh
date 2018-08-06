2ffa28440644c19f7e17d57df1b15c7aa18fa9ca
Antoine Delignat-Lavaud <antdl@microsoft.com>
2018-08-06 18:50:59 +0100

    More fixes for Windows integration

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=17af1612035fe7b71631f1fcbbf22579200905de
+hashes[mitls-fstar]=93344879e004bd91c138309d9e3e390aa010baaa
 branches[mitls-fstar]=master
 hashes[vale]=f0903cd82ef556d57d72305bdee7d6c4c4ec66f0
 branches[vale]=aes_bytes
@@ -8,7 +8,7 @@ hashes[FStar]=598b3dcb2ac9b88e11d811a5756b82f5445e9104
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=0a5f4cfc2f3c7d193547cad54c939bab41c108b9
+hashes[hacl-star]=b2ea29bd11fbf1e0e8b867a35c51acbfec7a54b0
 branches[hacl-star]=fstar-master
-hashes[kremlin]=3de22d33d7a04daab497573b6f4220156136c366
+hashes[kremlin]=12ee2beabcd0c1b6a5dacb8144cef8cdfde1df80
 branches[kremlin]=master
