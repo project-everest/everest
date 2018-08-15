683053b90d94c793fff1433c9ed4bb9398b19a3d
Dzomo the Everest Yak <everbld@microsoft.com>
2018-08-15 13:59:53 -0700

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -2,7 +2,7 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=ca7c0f99234b34c85686ee00eed6659be3046170
 branches[mitls-fstar]=master
-hashes[vale]=183a49af158ded42f5e966ae9e53395d86fc9286
+hashes[vale]=3b9c84f0b86315fcfb29f5fc4898753b547ed788
 branches[vale]=master
 hashes[FStar]=9bf36462825e763d33d9d1a5d5274929c4a128a7
 branches[FStar]=master
