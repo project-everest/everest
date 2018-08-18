f70a2b9d6d2da6150c2b1d57d289111e05b22cdd
Aseem Rastogi <aseemr@microsoft.com>
2018-08-18 15:35:01 +0530

    upgrading F* and mitls hashes

--- hashes.sh
+++ hashes.sh
@@ -1,10 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=7ca4afb0d1be0fd864f57422d8c3e295baa55bc3
+hashes[mitls-fstar]=7a8d1e251140b234a5e4ab6c8b4ca0ecd25a0113
 branches[mitls-fstar]=master
 hashes[vale]=3d3fcc0e650643d92da476af6e58d79c7f7ea147
 branches[vale]=master
-hashes[FStar]=639b8d7a677f0c68fabe5830ad525af98462db5b
+hashes[FStar]=69d9969a426feecfe49021c22bdb66f1a18b2e52
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
