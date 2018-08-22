b543fed2b45212a89cad6cd167f17a417fb4a187
Tahina Ramananandro <taramana@microsoft.com>
2018-08-22 15:42:12 -0700

    Update

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=db782e06f473484664af2a30a1b4fec2fad745c2
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=cc246cfe446464572b81cc456a74ced2dc8de9f7
+branches[mitls-fstar]=taramana_clean
 hashes[quackyducky]=3294416970d6e5c48e75239baa25ec47a95b88eb
 branches[quackyducky]=master
 hashes[vale]=2947d3b39bd30d1ae10ac6eef92454cf48edca97
 branches[vale]=master
-hashes[FStar]=e41101d40db10d7f0974ad258890fbaeb8ea2c42
+hashes[FStar]=c05f807fabdaf1be5c733e2184aabd439c4c9983
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=448854f8b14a15c6251e004fe25fc522507b16bf
+hashes[hacl-star]=3e84ca6692d21b283a12067810e77778697f65cb
 branches[hacl-star]=fstar-master
-hashes[kremlin]=c6206cf0cf006ef2e026dd39b129f153a9c5b9e6
+hashes[kremlin]=21d2be8145896aea37827d5a8a92a605220c3999
 branches[kremlin]=master
