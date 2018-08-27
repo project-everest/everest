c1f61c32e9a1865db9a54854d65fe32eaf4e4f3f
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-08-27 15:31:49 -0700

    Fix compilation of kremlib

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=41fa8e4ebc1502eae1fcbb0dd2d7a6503b82c5e6
+hashes[mitls-fstar]=0c4b22ab3effa81e9357ae9348f204ba55eb46ee
 branches[mitls-fstar]=master
-hashes[quackyducky]=3294416970d6e5c48e75239baa25ec47a95b88eb
+hashes[quackyducky]=f5431fcd842d70ce0453959efddfd09485787775
 branches[quackyducky]=master
 hashes[vale]=2947d3b39bd30d1ae10ac6eef92454cf48edca97
 branches[vale]=master
-hashes[FStar]=351dbdec0ec9f67fc2bbf57e787d14bdbd64bb22
+hashes[FStar]=f706b7380103b798c6f2986ed4a784652a2b836d
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=e6751509f9652262d4f5306a6d6ab9b8643ad611
+hashes[hacl-star]=5db806e1bd878e310679b209183246684f1dbc58
 branches[hacl-star]=fstar-master
-hashes[kremlin]=21d2be8145896aea37827d5a8a92a605220c3999
+hashes[kremlin]=c892fd96a3c524cbd3aa7d50ce8df2e7ad202b56
 branches[kremlin]=master
