9e66159eb0b2944b02954e061744e18b1761a54c
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-08-27 21:06:28 -0700

    Try again

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=0c4b22ab3effa81e9357ae9348f204ba55eb46ee
+hashes[mitls-fstar]=d8117f3f224ede89ffd6042ebffd1d08b3102bdc
 branches[mitls-fstar]=master
-hashes[quackyducky]=f5431fcd842d70ce0453959efddfd09485787775
+hashes[quackyducky]=ce2f8c3111fd67c7ffc07f7e6bc0deb7458e164a
 branches[quackyducky]=master
-hashes[vale]=2947d3b39bd30d1ae10ac6eef92454cf48edca97
+hashes[vale]=4919cb564d20f0bd7e8ffc6473aa971549ac75ab
 branches[vale]=master
-hashes[FStar]=f706b7380103b798c6f2986ed4a784652a2b836d
+hashes[FStar]=6fa0df890375ad9a3126fc9bf7b21954b11c7175
 branches[FStar]=master
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
 hashes[hacl-star]=5db806e1bd878e310679b209183246684f1dbc58
 branches[hacl-star]=fstar-master
-hashes[kremlin]=c892fd96a3c524cbd3aa7d50ce8df2e7ad202b56
+hashes[kremlin]=300c8020173514ec0f47f33e26f4fa867be11302
 branches[kremlin]=master
