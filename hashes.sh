7e3ec0538e1800ae7b1a988ab7c428319481a133
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-08-28 14:44:32 -0700

    Try these revisions

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=d8117f3f224ede89ffd6042ebffd1d08b3102bdc
+hashes[mitls-fstar]=2dfde337724ea02d548e2412404c5f582ad37547
 branches[mitls-fstar]=master
 hashes[quackyducky]=ce2f8c3111fd67c7ffc07f7e6bc0deb7458e164a
 branches[quackyducky]=master
@@ -10,7 +10,7 @@ hashes[FStar]=8cb00fdc82c551d3404aa6ed80266278b4ba6a77
 branches[FStar]=protz_mod
 hashes[MLCrypto]=048fd71bf111e5020e6f47d884741d595cdb9ab3
 branches[MLCrypto]=master
-hashes[hacl-star]=5db806e1bd878e310679b209183246684f1dbc58
-branches[hacl-star]=fstar-master
-hashes[kremlin]=300c8020173514ec0f47f33e26f4fa867be11302
+hashes[hacl-star]=b0b597a9154bfcda49ef265001ab84de06f59c5a
+branches[hacl-star]=protz_sha
+hashes[kremlin]=40c6ceb8e15eada785b3447de3523339df7de8cb
 branches[kremlin]=master
