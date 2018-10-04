4eda94b452c0504d066e3947f9f00b73c4b69e98
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-10-04 05:46:22 -0700

    Try with the init_slice lemma change

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=d1492ff95689c5e47e17b31f514a463eb51ce732
+hashes[mitls-fstar]=402a65d9a47ead15ebcb9df41a060fe541be5644
 branches[mitls-fstar]=master
 hashes[quackyducky]=6c15bdbbc63046e919c3c3ba822d8f6ae62078bb
 branches[quackyducky]=master
 hashes[vale]=59dce69840e04f95c32e3cbc821a4fb0dadf8aab
 branches[vale]=master
-hashes[FStar]=67d0293c1b29c4bd5725e3ae8ae043ec0d369024
-branches[FStar]=master
+hashes[FStar]=96e88aff931f87e56ffdfe497affd1cd3f969e01
+branches[FStar]=protz_smtpat_fix
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=080f7e26c079b91ba391c4d08855455be1fcbb88
+hashes[hacl-star]=55a4e72e4e958331c3b356ab158df1e071d68fb2
 branches[hacl-star]=fstar-master
-hashes[kremlin]=91ee739ef5a4fbf3e207733d81a4f415e1ec39d3
+hashes[kremlin]=d9596fad22f6d062c4c1d86573f161bd922b8dff
 branches[kremlin]=master
