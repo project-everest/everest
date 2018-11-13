f532bf1bba21995bb8232702f00d17cb35fbd591
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-11-13 14:56:27 -0800

    try again

--- hashes.sh
+++ hashes.sh
@@ -1,7 +1,7 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=06d50acee2e6f0abb42b201aa5cbc1cf1d58e7f
-0branches[mitls-fstar]=protz_hash_integration
+hashes[mitls-fstar]=06d50acee2e6f0abb42b201aa5cbc1cf1d58e7f0
+branches[mitls-fstar]=protz_hash_integration
 hashes[quackyducky]=91a935e534709aaf2e6b6703718bcfca4c60db3a
 branches[quackyducky]=master
 hashes[vale]=a94b616d1fba0b95649ff20378c17105a6ad3cdb
