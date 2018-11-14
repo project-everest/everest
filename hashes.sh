06b92daacd6dd835c13e2fcc601ae4b5384e3372
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-11-14 11:04:56 -0800

    Try removing sed from the TCB

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=d7fe0409e02953edea143565a7d89c667cc4a482
+hashes[mitls-fstar]=f239802350923e76247e90839683f7f1063c29d8
 branches[mitls-fstar]=protz_hash_integration
 hashes[quackyducky]=91a935e534709aaf2e6b6703718bcfca4c60db3a
 branches[quackyducky]=master
-hashes[vale]=a94b616d1fba0b95649ff20378c17105a6ad3cdb
+hashes[vale]=2cf07174b7f0bbe5702e6f1e99aacba0a10502f8
 branches[vale]=master
-hashes[FStar]=20b0ef349114fd665e750c92d6b0f008b421da9a
+hashes[FStar]=88c7053f9f70cfd991c1d9ac877dc60a234fef9d
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=5ff6acd1d3741ad57af5cbee55e9544d620c483f
+hashes[hacl-star]=d98741c7c7e43ed6a335751160554fbedbdb717d
 branches[hacl-star]=protz_hash_integration
 hashes[kremlin]=d94ac163e1258cc291cff969709815bc76c961c6
 branches[kremlin]=master
