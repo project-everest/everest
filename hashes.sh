4f26a659bef06414d65402fc4c4187b4f797decc
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-12-07 10:01:09 -0800

    successful build on my machine

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=9b163597f41de1b614f14ca9ca2f9c486a7894bc
+hashes[mitls-fstar]=316c3bf3bbdef550cad704854e72bb0ad0a7d938
 branches[mitls-fstar]=protz_bundling
-hashes[quackyducky]=20a9b7d3f21b4d40ac29074d9b88862c21a95786
+hashes[quackyducky]=c70ea695dcaacb106d26efe512b2ed00a6ec4457
 branches[quackyducky]=master
-hashes[vale]=c6e4545f1a2e4b5c062429e6da5cecad7a578033
+hashes[vale]=2cf07174b7f0bbe5702e6f1e99aacba0a10502f8
 branches[vale]=master
-hashes[FStar]=8f0ddba42fcba456c5b09717deb302e29fac7c42
+hashes[FStar]=bf34207da7f5b34ff786cd10720fb398ffcec64a
 branches[FStar]=protz_bundling2
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=22993c9f36c8da0601803fd08fcaf10564261ffb
+hashes[hacl-star]=c3c7973a8ac83ed29c85e134eec8eb98d35f5841
 branches[hacl-star]=protz_bundling
 hashes[kremlin]=9768dccd3277b1e144f6810809b3b7844a5f76e6
 branches[kremlin]=protz_bundling
