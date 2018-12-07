2323c240b5de78fd9615c42862b3e127c00fd178
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2018-12-07 07:14:49 -0800

    update everything, try again

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=db937e2ea68065b0492b2530760108a14f376c18
+hashes[mitls-fstar]=0626cbb4fd5c89af025a49aa9363834922e99e7a
 branches[mitls-fstar]=protz_bundling
-hashes[quackyducky]=20a9b7d3f21b4d40ac29074d9b88862c21a95786
+hashes[quackyducky]=c70ea695dcaacb106d26efe512b2ed00a6ec4457
 branches[quackyducky]=master
-hashes[vale]=2cf07174b7f0bbe5702e6f1e99aacba0a10502f8
+hashes[vale]=c6e4545f1a2e4b5c062429e6da5cecad7a578033
 branches[vale]=master
 hashes[FStar]=8f0ddba42fcba456c5b09717deb302e29fac7c42
 branches[FStar]=protz_bundling2
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=f5d3db637355f96ff22d3b8343f5b368f63fa1c1
+hashes[hacl-star]=22993c9f36c8da0601803fd08fcaf10564261ffb
 branches[hacl-star]=protz_bundling
 hashes[kremlin]=9768dccd3277b1e144f6810809b3b7844a5f76e6
 branches[kremlin]=protz_bundling
