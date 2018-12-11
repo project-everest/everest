1087c66ac784a12df129b56ceee774a1b1624600
Jonathan Protzenko <protz@microsoft.com>
2018-12-11 11:13:13 -0800

    successful msvc build

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=72c171d1db5fa2729a0379000c34f101f5daae1b
+hashes[mitls-fstar]=2779a06922660c4a8cebb46dc7490cc63ddb3c25
 branches[mitls-fstar]=master
 hashes[quackyducky]=c70ea695dcaacb106d26efe512b2ed00a6ec4457
 branches[quackyducky]=master
-hashes[vale]=2cf07174b7f0bbe5702e6f1e99aacba0a10502f8
+hashes[vale]=c6e4545f1a2e4b5c062429e6da5cecad7a578033
 branches[vale]=master
-hashes[FStar]=6be8684d68a5075962efbe5657bcb6e9d9bba74e
+hashes[FStar]=37083417ea9685e901fe69ede82b13b259d65458
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=ed4ebd2a2758f72a139904e8e80e278ebde90815
+hashes[hacl-star]=031b3beb79f01dbb9d49e60368fc5872fa7ad785
 branches[hacl-star]=fstar-master
 hashes[kremlin]=c61eec1d377bf6bd6b95980c122a266e8ae5dc90
 branches[kremlin]=master
