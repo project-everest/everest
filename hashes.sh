d714e741f6e0299c85d4c3f708c4a2dc37de16c9
Jonathan Protzenko <protz@microsoft.com>
2018-12-20 19:59:30 -0800

    minor update to the drop script + hashes

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=0c979ee38721530c8354c02f711bf2a1cbbc3b9c
+hashes[mitls-fstar]=58ccf019397416403b2eb7196615a02aaef472bc
 branches[mitls-fstar]=master
 hashes[quackyducky]=d235c9bb41202c7abf778fc81e6d517bc183f780
 branches[quackyducky]=master
 hashes[vale]=cc050b145259e9190fc04c782636cde09457885c
 branches[vale]=master
-hashes[FStar]=30801ec5bb7dfa867ef166d74b9b2684cb1f5db1
+hashes[FStar]=cf61b65316bab3b3cd74fe22f6d6f36675b8370b
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=7959ea55738f40bb1bc2dc2b9b297f7c96156e0c
+hashes[hacl-star]=110329335
 branches[hacl-star]=fstar-master
-hashes[kremlin]=062c33e07d395b6d705906cae2f02bf75360faf7
+hashes[kremlin]=ea5b15479fe7b7ae4a359ca217b21a34e0fd77b2
 branches[kremlin]=master
