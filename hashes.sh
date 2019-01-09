d207a280de87cf430b5f95a23e0849bbcceccabb
Aseem Rastogi <aseemr@microsoft.com>
2019-01-09 16:14:35 +0530

    Revert "manually updaing the hashes"
    This reverts commit 7fd2c5774f3b0d51ff34a36c67286f80b7dde5d3.

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=4e0170d3af275690aac4445eaf7ef6d98153aecb
+hashes[mitls-fstar]=ed28469b6d79123493e3fe8a9b9be1b37194f144
 branches[mitls-fstar]=master
 hashes[quackyducky]=d235c9bb41202c7abf778fc81e6d517bc183f780
 branches[quackyducky]=master
 hashes[vale]=ebd58d1deabb56d40ee78b321edfea8bb3e0e81d
 branches[vale]=master
-hashes[FStar]=d4512c21216984639858bc892479cde1c8c5bc66
+hashes[FStar]=95984a8067e8be3e2cc02d9a43302a32360284db
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=62dd10bc64ed3be0adcf95153c8c7fa6069ee620
+hashes[hacl-star]=38f27765380c1b1b85d0728d0cd60a6d80b3656e
 branches[hacl-star]=fstar-master
-hashes[kremlin]=05f5a84d940128e82b5c5932e01da2d9f4b85978
+hashes[kremlin]=d54a60183ffb899b303998d33e9a9b5c0092c23e
 branches[kremlin]=master
