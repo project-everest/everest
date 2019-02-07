0b44e043e2c5bc0585d0f4ee724b93701bd2d4a8
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-02-07 14:28:20 -0800

    trying an upgrade with nik_thunk_top_level_axioms

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=4b6b5c3e5c70e0f905a32c65fa231ae6fdc32c4f
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=a40449437ee1b8b8768fbefeb51f25ba9275c2c4
+branches[mitls-fstar]=nik_fstar-master
 hashes[quackyducky]=a9e4f96faa9c24cb8c6d146f8a880fe3522c956a
 branches[quackyducky]=master
 hashes[FStar]=5d61a616a4c38f119ad745d3ab61aec582fe6f5a
 branches[FStar]=nik_thunk_top_level_axioms
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=b9f9a0854f746e5a5412f1a5d1e5b10dfd64376e
-branches[hacl-star]=fstar-master
+hashes[hacl-star]=69e032b99c90ee71dcc175b9ffa0cdd17b4500b6
+branches[hacl-star]=nik_fstar-master
 hashes[kremlin]=d8ba3898f9b0269b671a72d6e8ed0dff75371965
 branches[kremlin]=master
