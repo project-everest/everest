2f41d7e884fcb0e0a45292be2f7cd84a18de25ce
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-02-07 22:59:46 -0800

    snap

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=74683e4689200294f6f7289bc61052ca96607666
+hashes[mitls-fstar]=598cf5dce1c4d2f8508ea7032468af0211f149f2
 branches[mitls-fstar]=master
 hashes[quackyducky]=a9e4f96faa9c24cb8c6d146f8a880fe3522c956a
 branches[quackyducky]=master
-hashes[FStar]=d37e991bb6d0b6e9b3f66b3ec7ee520713a0223e
-branches[FStar]=master
+hashes[FStar]=5d61a616a4c38f119ad745d3ab61aec582fe6f5a
+branches[FStar]=nik_thunk_top_level_axioms
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=f48bb588e1ee2641b40d7cc87f313756d119511d
+hashes[hacl-star]=579e6cde785997a9a53165fc3af41205299e0acb
 branches[hacl-star]=fstar-master
 hashes[kremlin]=d8ba3898f9b0269b671a72d6e8ed0dff75371965
 branches[kremlin]=master
