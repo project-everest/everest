20ff01514e418b1f1e6232b935482fd297c8d9a3
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-02-08 07:36:52 -0800

    snap

--- hashes.sh
+++ hashes.sh
@@ -1,10 +1,10 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=598cf5dce1c4d2f8508ea7032468af0211f149f2
+hashes[mitls-fstar]=590ad75141ad8375d83297455274d13c37b269cd
 branches[mitls-fstar]=master
 hashes[quackyducky]=a9e4f96faa9c24cb8c6d146f8a880fe3522c956a
 branches[quackyducky]=master
-hashes[FStar]=5d61a616a4c38f119ad745d3ab61aec582fe6f5a
+hashes[FStar]=a6684d0253e0361fd34665cb0e70a8d3fabfcd23
 branches[FStar]=nik_thunk_top_level_axioms
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
