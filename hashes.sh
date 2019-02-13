d4e0a5aa3d542074299ae31e573d44f9066aa571
Jonathan Protzenko <protz@microsoft.com>
2019-02-13 15:47:24 -0800

    phew

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=45bacf7e14f93ee4f89250d943e1533d10a2c39b
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=78486e4d1d76a6ea6bae8940ef34c50c10543707
+branches[mitls-fstar]=protz_makefile2
 hashes[quackyducky]=d6b1a6eced0aa0913045594a23ef17fa1bdbbbfe
 branches[quackyducky]=master
 hashes[FStar]=41e423e53ca468c81af1373406a4ce42bd2d70f1
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=c470f888a0e71b4c2fea69b03907b72813cb34a0
-branches[hacl-star]=fstar-master
+hashes[hacl-star]=0e5b49d98659877d3c3d91eecfddaa84cd6ae7d4
+branches[hacl-star]=protz_makefile2
 hashes[kremlin]=d8ba3898f9b0269b671a72d6e8ed0dff75371965
 branches[kremlin]=master
