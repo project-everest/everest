a386cffc4ab586e53cdbc8da25052df78a071238
Tahina Ramananandro <taramana@microsoft.com>
2019-03-23 10:41:55 -0700

    Use latest miTLS and QuackyDucky master

--- hashes.sh
+++ hashes.sh
@@ -1,9 +1,9 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=103c518401679bcd801fa8b2bdc0879220aa74ea
-branches[mitls-fstar]=taramana_everparse
+hashes[mitls-fstar]=856cff5d800ea2a401e99052d304656caa343e1e
+branches[mitls-fstar]=master
 hashes[quackyducky]=d5178912dbc473ea18ed56dc06685ae108fb8035
-branches[quackyducky]=taramana_everparse
+branches[quackyducky]=master
 hashes[FStar]=1820e020f16fb00ddc8565a18a3ed7be868e73b1
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
