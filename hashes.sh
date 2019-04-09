51377a70052ad61df7eabc3ee56e93db20bb6c60
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-04-08 18:10:56 -0700

    try nik_valid_intro

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=acff7849d6198d40568564465e8b4a694ab5df11
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=769332a51b182b340011dd507a79fd49f516da5e
+branches[mitls-fstar]=nik_validity_axioms
 hashes[quackyducky]=1f62628fe0e724582dd95102006a8a003e9955d1
 branches[quackyducky]=master
-hashes[FStar]=66d92e8a21ae8f02f39970d7c1d080d6eddabe94
-branches[FStar]=master
+hashes[FStar]=dcca5683f3dbfbbb4e3ce24f49d0371c09c0beed
+branches[FStar]=nik_valid_intro
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=cefa9fab01a625225fb9cc39ae50eeb5eeb618fd
-branches[hacl-star]=fstar-master
+hashes[hacl-star]=2f522b73a954cba616a062069b5b130814f77b8b
+branches[hacl-star]=nik_validity_axioms
 hashes[kremlin]=8e2499453d4abf04996147b735157eb4c62088b5
 branches[kremlin]=master
