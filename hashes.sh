38a4727bb8c2ae3d958c31e891036cbb9187f5cd
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-06-11 08:04:30 -0700

    trying a full z3-4.8.5 build again

--- hashes.sh
+++ hashes.sh
@@ -2,10 +2,10 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=2eca06ead940f0a6c65ce81770067781777362f4
 branches[mitls-fstar]=master
-hashes[quackyducky]=2718b70d1effc35f928d0d8dbc859510eada69c5
+hashes[quackyducky]=56c09984151be2342f2fbe10b1f88ec6071d3cdd
 branches[quackyducky]=master
-hashes[FStar]=4cb70a99f941f486c21a2cca16c9be4ab3824e4f
-branches[FStar]=master
+hashes[FStar]=5c1bcb6177c0cd81da772542ac9382043fe417c3
+branches[FStar]=z3-4.8.5
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[hacl-star]=337fa4f585628726ab2cd6264f8d9f3ecc2ce739
