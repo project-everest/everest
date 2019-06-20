925d2eab1b1ec88b767fed977d6bbd7251f19f8a
Tahina Ramananandro <taramana@microsoft.com>
2019-06-19 18:40:16 -0700

    Use QuackyDucky `master` branch

--- hashes.sh
+++ hashes.sh
@@ -3,7 +3,7 @@ declare -A branches
 hashes[mitls-fstar]=a62687f6d5219ff9ea3773d57052c8805d6c5473
 branches[mitls-fstar]=dev
 hashes[quackyducky]=c7142559976492082cb51b96778618aa4cf2d9bd
-branches[quackyducky]=dev
+branches[quackyducky]=master
 hashes[FStar]=4e241642dee1f4e22d4a259bf1df36135e39e41a
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
