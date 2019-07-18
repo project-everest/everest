7886a0c776f08b713b53f6b67e95ff7ce94433a1
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-07-17 19:10:52 -0700

    try again

--- hashes.sh
+++ hashes.sh
@@ -2,13 +2,13 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=37e932ea5c97c09c606aa145dd0f916a96eb77bf
 branches[mitls-fstar]=master
-hashes[quackyducky]=827e737508f68b1b231b29d9fe14dddffc4b761e
+hashes[quackyducky]=3095d52d9c47b19c42b942f4362ff2262e4aebd0
 branches[quackyducky]=master
 hashes[FStar]=c4a6d0038033ef516c8085275456b1c37ac949d7
 branches[FStar]=nik_strict
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=32de512ffde22614a29c480d91e0151f2639e462
-branches[hacl-star]=fstar-master
+hashes[hacl-star]=2ea5ab31da988b7996c1d4b6ac7a53fa7ca96b43
+branches[hacl-star]=nik_1815
 hashes[kremlin]=205f888465d6649cb35831d4fcd0b4c979502191
 branches[kremlin]=master
