054e5f2cb7ba131257642fdd73938c4f5d2c2669
Dzomo the everest Yak <everbld@microsoft.com>
2019-07-22 08:13:59 -0700

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -2,13 +2,13 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=a8dc7b0529c7dc7c21b6551c3b1214acba2c13b4
 branches[mitls-fstar]=master
-hashes[quackyducky]=b3478870c60fcdc65f9d63c7936a54e1f795d6b7
+hashes[quackyducky]=65e39cdeb516188cd4a843c37e9536e98b6fc68c
 branches[quackyducky]=master
-hashes[FStar]=2537b4b345a976cf1bf30df9e32ec4a00c56a928
+hashes[FStar]=9540a4fad1f08d21b0d6fb4c4a32b3d6d30ccad5
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=802e4abbdce467c1f35f84c9d1dd64aa61b021b6
+hashes[hacl-star]=5fe63b4875cd34cb938744a40d8b9d7446d57f1f
 branches[hacl-star]=fstar-master
 hashes[kremlin]=f7f0a7a03df927d90fcc69c28d1d657afac9159c
 branches[kremlin]=master
