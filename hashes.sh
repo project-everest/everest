b92e5c61f45f9985dad1cf7cc49f85076d35f6f5
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-09-12 15:12:02 -0700

    trying nik_erasable again with smt-tuning patches to vale and lowparse

--- hashes.sh
+++ hashes.sh
@@ -2,13 +2,13 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=cac625866850f588d4a1487eb5d43c2075d3f63d
 branches[mitls-fstar]=master
-hashes[quackyducky]=ef41386ff68cf557fcca4d6f9531a81dd89896d6
+hashes[quackyducky]=3020b8122dc45545f6a2c24b699f4d80880efa93
 branches[quackyducky]=master
 hashes[FStar]=df17c771ee50467879d8a8a9b6ac949790b59904
 branches[FStar]=nik_erasure
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=ae15f161d496ea7979a328082072fc9d86be7b20
-branches[hacl-star]=fstar-master
+hashes[hacl-star]=c2132714d1618dfb19fd13f76c9545e9c0cf418a
+branches[hacl-star]=nik_erasable
 hashes[kremlin]=fec2dd6f2c3f230368d25b8563f29b590216130e
 branches[kremlin]=master
