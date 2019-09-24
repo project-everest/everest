6a23aa5bba7b89bcb751f8c5b21fd0a5566b9bf2
Nikhil Swamy <nikswamy@users.noreply.github.com>
2019-09-24 12:44:25 -0700

    try with a kremlin makefile fix

--- hashes.sh
+++ hashes.sh
@@ -10,5 +10,5 @@ hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[hacl-star]=a447e5c412bf8d04e7c78c4fdaa4d4869655e240
 branches[hacl-star]=fstar-master
-hashes[kremlin]=c04d180288897d5bd8068a7e59bd12b5c447541c
+hashes[kremlin]=f6299a2aab5ad2b6a66048d22625033def041ad3
 branches[kremlin]=master
