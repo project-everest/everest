2b8c582ad4faaae05dcfb5b59479d34a80593633
Santiago Zanella-Beguelin <santiago@microsoft.com>
2019-10-24 13:55:36 +0100

    Update mitls-fstar hash

--- hashes.sh
+++ hashes.sh
@@ -1,13 +1,13 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=87c1163c3a7b46d13f2cef84f2027a4c8d848819
+hashes[mitls-fstar]=d011f980fa3ec302fd01342fbe1ad32bb87dcf8a
 branches[mitls-fstar]=dev
+hashes[quackyducky]=152bbf79e55dc2d556096105301c6abb72458587
+branches[quackyducky]=master
 hashes[FStar]=29da2e71c402d302ccab6129f87f8034c45e9394
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[quackyducky]=152bbf79e55dc2d556096105301c6abb72458587
-branches[quackyducky]=master
 hashes[hacl-star]=f5542c56c39345d60278306fef178027ac9fd9f3
 branches[hacl-star]=fstar-master
 hashes[kremlin]=e324b7e63e24d0822e753051b9dc7f7ab294bc00
