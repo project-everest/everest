624f910e4e76d576700f68fad9d20ae2c5afe199
Jonathan Protzenko <protz@microsoft.com>
2019-11-26 10:00:13 -0800

    this builds on my windows machine

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
+hashes[mitls-fstar]=89c43b1b76f5cb3d1772299e935bd03bd1c16702
+branches[mitls-fstar]=protz_static
 hashes[quackyducky]=e7f62911450b9bfc8fe7bea8a10079a6d3004d4d
 branches[quackyducky]=master
-hashes[kremlin]=6f34522ab66c7a9528a103143e5196aa80c68296
-branches[kremlin]=protz_static
-hashes[hacl-star]=f6672a2a17de8f0d67e5a8b10e02b7808fed6b77
-branches[hacl-star]=fstar-master
-hashes[mitls-fstar]=94237c658e24c2d10ce556d5932be4363289356c
-branches[mitls-fstar]=protz_static
-hashes[FStar]=62bac435a413ad8502544bb0942e726ff502553e
+hashes[FStar]=08e934baa03c23f6e3951cc319487a22fb9af632
 branches[FStar]=protz_static
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
+hashes[hacl-star]=3c44008ca53416ad27812577307ef559a62678db
+branches[hacl-star]=protz_static
+hashes[kremlin]=4e933dcf77085679140155a5e2b452a10e3d688e
+branches[kremlin]=protz_static
