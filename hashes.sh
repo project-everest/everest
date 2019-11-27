869182eb71e168f1af2b26c16df96619567b4c65
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2019-11-27 09:57:01 -0500

    point to master

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=2c4e44ba0bff2848c18ce1cbaec4a6afee2aa332
-branches[mitls-fstar]=protz_dev_static
+branches[mitls-fstar]=dev
 hashes[quackyducky]=e7f62911450b9bfc8fe7bea8a10079a6d3004d4d
 branches[quackyducky]=master
 hashes[FStar]=5d4d580432939d195649b28d6969ff71b1c53b27
-branches[FStar]=protz_static
+branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[hacl-star]=b8bf24bd5ee13fd7656d601f3dae0e497b7a435a
-branches[hacl-star]=protz_static
+branches[hacl-star]=fstar-master
 hashes[kremlin]=9c7a07cb87d99d14e8494cccf3524f6caf1cec9c
-branches[kremlin]=protz_static
+branches[kremlin]=master
