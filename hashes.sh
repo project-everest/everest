d2381687bff09fc271656dc18d2c2fd3b744f842
Jonathan Protzenko <protz@microsoft.com>
2019-11-26 18:38:48 -0800

    try dev

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=d011f980fa3ec302fd01342fbe1ad32bb87dcf8a
-branches[mitls-fstar]=dev
-hashes[FStar]=908367fe9bec434ae6c9f6d80e38ce8b0609f2de
-branches[FStar]=master
-hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
-branches[MLCrypto]=master
+hashes[mitls-fstar]=bc3144adb4b63eead5a1499b34c282965f47d21f
+branches[mitls-fstar]=protz_dev_static
 hashes[quackyducky]=e7f62911450b9bfc8fe7bea8a10079a6d3004d4d
 branches[quackyducky]=master
-hashes[hacl-star]=3d3a8bcbd149a9e99ff8e87efeeb142226a6fa32
-branches[hacl-star]=fstar-master
-hashes[kremlin]=d51b905ac0b1ae83c4ba7f0a13e0c94504b67c10
-branches[kremlin]=master
+hashes[FStar]=5d4d580432939d195649b28d6969ff71b1c53b27
+branches[FStar]=protz_static
+hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
+branches[MLCrypto]=master
+hashes[hacl-star]=b8bf24bd5ee13fd7656d601f3dae0e497b7a435a
+branches[hacl-star]=protz_static
+hashes[kremlin]=9c7a07cb87d99d14e8494cccf3524f6caf1cec9c
+branches[kremlin]=protz_static
