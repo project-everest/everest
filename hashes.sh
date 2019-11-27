07fb5b48278cf564aaf90591871ab91cbaf84fd5
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2019-11-27 08:46:03 -0500

    known working hashes

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=89c43b1b76f5cb3d1772299e935bd03bd1c16702
-branches[mitls-fstar]=protz_static
+branches[mitls-fstar]=master
 hashes[quackyducky]=e7f62911450b9bfc8fe7bea8a10079a6d3004d4d
 branches[quackyducky]=master
 hashes[FStar]=08e934baa03c23f6e3951cc319487a22fb9af632
-branches[FStar]=protz_static
+branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[hacl-star]=ba51f457880b8f64263b66d6d77423187da35ed1
-branches[hacl-star]=protz_static
+branches[hacl-star]=master
 hashes[kremlin]=1c6d2c0c4c8bccf2f727db083b099a5f56238204
-branches[kremlin]=protz_static
+branches[kremlin]=master
