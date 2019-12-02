d07c9c8698a413e04daeffeb8158fdbb672ece3e
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2019-12-02 12:24:37 -0500

    Catch up on latest HACL* renamings

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=2c4e44ba0bff2848c18ce1cbaec4a6afee2aa332
+hashes[mitls-fstar]=e333b5848508974d8d22f5a196de0c5b277b8520
 branches[mitls-fstar]=dev
 hashes[FStar]=cf1abbb281ff905f8b2f3a3c24ea4b0edcae514d
 branches[FStar]=master
@@ -8,7 +8,7 @@ hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[quackyducky]=7baa13fd44bb0fc9ec63ce0e9e8509deb9f8aea0
 branches[quackyducky]=master
-hashes[hacl-star]=911eea9c7701fcb6a7e3ff0f92bc93a7bee6eb8e
+hashes[hacl-star]=bc6c1f1832a0ae544754817d84ce9f5684b292e6
 branches[hacl-star]=fstar-master
 hashes[kremlin]=dc80e6cc827754952e49793a77896239843cadf0
 branches[kremlin]=master
