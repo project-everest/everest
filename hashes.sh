a15381aec8ef2edce0b1665f2c1eb291bc4c643c
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2020-01-08 16:39:33 -0500

    Try short names

--- hashes.sh
+++ hashes.sh
@@ -10,5 +10,5 @@ hashes[quackyducky]=7522e46e17a9885fad754847ae7261629a80211e
 branches[quackyducky]=master
 hashes[hacl-star]=91d8bf1ff4d232cb51fcc7b6de9a8d83ec81a5de
 branches[hacl-star]=fstar-master
-hashes[kremlin]=ffa9901aa48e34c718d5a90383d22056949b4cb1
-branches[kremlin]=master
+hashes[kremlin]=a5160ec1ade7757dd0f89371f65303480aad7698
+branches[kremlin]=protz_short_names
