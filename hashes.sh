a75b000970c2ac3b0d7e4799866a087041d0207f
Dzomo the everest Yak <everbld@microsoft.com>
2020-01-09 13:03:45 -0800

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -10,5 +10,5 @@ hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[hacl-star]=2e5330e233189d58fb38af4f93785b30b2b3be84
 branches[hacl-star]=fstar-master
-hashes[kremlin]=b56437351bd8488aeec04da486ff2e7e28a04d86
+hashes[kremlin]=f2a2069f21b650343dc8b31c026c3cd45faaaf15
 branches[kremlin]=master
