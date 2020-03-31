cb69e4f4d060f73779cd6cef6f30ea4d754b3526
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2020-03-31 10:27:44 -0700

    Followup on changes in HACL* protz_noopenssl

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=0ec7023a43d566d36d9d491f82fc8649116e9217
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=7ce64fce87d1d23c40c0232ea96e78437b7dc62f
+branches[mitls-fstar]=protz_noopenssl
 hashes[quackyducky]=816d1774f0c3f21dd2bb09f33c07723b33a5945f
 branches[quackyducky]=master
 hashes[FStar]=244d3f3584b9d554e24ac4604e56e62397f2641f
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=ccff3c3374ae4e8a3dc9057cc49a357c40f9f738
-branches[hacl-star]=master
+hashes[hacl-star]=4f4b3295fda1c6d52ba43760fb2874888bec26e8
+branches[hacl-star]=protz_noopenssl
 hashes[kremlin]=d10fb0cdedb3772b6d06395b8d5654dcd75693e1
 branches[kremlin]=master
