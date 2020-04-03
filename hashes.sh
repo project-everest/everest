5597f514f9b52ccb34ea1ccbc5250ca4d7323988
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2020-04-02 17:48:22 -0700

    fix branches

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=7ce64fce87d1d23c40c0232ea96e78437b7dc62f
-branches[mitls-fstar]=protz_noopenssl
+hashes[mitls-fstar]=e812406cde2fbecafd6abc6375f7ca3f29cab403
+branches[mitls-fstar]=master
 hashes[quackyducky]=816d1774f0c3f21dd2bb09f33c07723b33a5945f
 branches[quackyducky]=master
 hashes[FStar]=244d3f3584b9d554e24ac4604e56e62397f2641f
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=4f4b3295fda1c6d52ba43760fb2874888bec26e8
-branches[hacl-star]=protz_noopenssl
+hashes[hacl-star]=4841036956ae29aa0a6f6cc9be63de6744d846eb
+branches[hacl-star]=master
 hashes[kremlin]=d10fb0cdedb3772b6d06395b8d5654dcd75693e1
 branches[kremlin]=master
