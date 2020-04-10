4f67ac73bddf253a855175e843543ac8a0651e22
Guido Martínez <mtzguido@gmail.com>
2020-04-10 17:19:59 -0300

    sort hashes.sh

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=d2f30b1a0a8572c7cba9adb87f9372b242966474
-branches[mitls-fstar]=master
-hashes[quackyducky]=d7b9cce54d881ec75d11c6bc7c00b67fb42385a2
-branches[quackyducky]=master
 hashes[FStar]=817fb359eaf7ed6a622ee2e509cabf4c361b2688
 branches[FStar]=master
-hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
-branches[MLCrypto]=master
 hashes[hacl-star]=5fcfae28a4bda77dd62649d051a6abc6896e22c7
 branches[hacl-star]=master
 hashes[kremlin]=6dc88cee6122b7b903c6df8690c2f3ef736b80e6
 branches[kremlin]=master
+hashes[mitls-fstar]=d2f30b1a0a8572c7cba9adb87f9372b242966474
+branches[mitls-fstar]=master
+hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
+branches[MLCrypto]=master
+hashes[quackyducky]=d7b9cce54d881ec75d11c6bc7c00b67fb42385a2
+branches[quackyducky]=master
