ed5846ec11ac0815668272975e800487405dae4f
Dzomo the everest Yak <everbld@microsoft.com>
2020-07-30 02:42:14 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=29285e9d14293643f38285c2fe428a6ef04453b3
+hashes[FStar]=cdd09a99ee3e44c1494cd1e0519f5c3e7b7d4b52
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
