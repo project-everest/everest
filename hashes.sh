eb47992d811fc46e231838d13c0d759fa6dbb3d7
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2020-08-04 16:25:07 -0700

    Try again

--- hashes.sh
+++ hashes.sh
@@ -2,13 +2,13 @@ declare -A hashes
 declare -A branches
 hashes[FStar]=7ae75402973fa5f32afebab510f14a9319b6b0c3
 branches[FStar]=master
-hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
-branches[MLCrypto]=master
 hashes[hacl-star]=b1e8c79969c5e8bdc34fe5a5ffe53071bd5b20bf
 branches[hacl-star]=master
-hashes[kremlin]=f3afecc8
+hashes[kremlin]=c641cb5257c57026fd6a73eb3e1dfd13ada2910e
 branches[kremlin]=protz_names
-hashes[mitls-fstar]=0f14aa2e3
+hashes[mitls-fstar]=0f14aa2e30e0b5641ddc4caa34c8a8acf46dc8fb
 branches[mitls-fstar]=protz_names
+hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
+branches[MLCrypto]=master
 hashes[quackyducky]=76b4cbb473b001e26a88b729df61362779905bb9
 branches[quackyducky]=master
