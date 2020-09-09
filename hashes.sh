4c4b76f50496ed9f073112eb3c6f8c62539f7379
Nikhil Swamy <nikswamy@users.noreply.github.com>
2020-09-09 12:13:09 -0700

    try F* normalizer patch for zeta_full

--- hashes.sh
+++ hashes.sh
@@ -1,7 +1,7 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=0050821b43671f2c40318813b053b706e2435136
-branches[FStar]=master
+hashes[FStar]=736867bfc3093500a737d7fe4a947e39c2b5f853
+branches[FStar]=nik_zeta_full_norm_cfg
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[hacl-star]=fa7d5068ebd6d67e9d752e4c4f81aa7652f1641f
