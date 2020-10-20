e61cacfdce978813504d9bb5163d430699750367
Dzomo the everest Yak <everbld@microsoft.com>
2020-10-20 08:58:27 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=d3d4e120f693ba88b0348af1f8324da8292b9386
+hashes[FStar]=2697203695c0e6df30ac679a9005eb8f68cdbb9f
 branches[FStar]=master
 hashes[hacl-star]=ba74e7571565f727c9a56f0fb4ce1e4170c07ae0
 branches[hacl-star]=master
 hashes[kremlin]=f3656741e080d038f5f96a2864666e58ba34b831
 branches[kremlin]=master
-hashes[mitls-fstar]=ebb8c0290bb5149c7c09decb91141882611024ed
+hashes[mitls-fstar]=d89dec6d765296423db888f39b498f61888e4b21
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
