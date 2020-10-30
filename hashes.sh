10b83fb44fba7f067b55e5b57f1fd44536852865
Aymeric Fromherz <fromherz@cmu.edu>
2020-10-30 12:14:20 -0400

    hashes imitate app

--- hashes.sh
+++ hashes.sh
@@ -1,7 +1,7 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=d570574963d777355e2c3043b4d00516ffc75346
-branches[FStar]=master
+hashes[FStar]=4c23b37ec4242d3a9bdad073d947951bbda0f9eb
+branches[FStar]=afromher_imitate_app
 hashes[hacl-star]=5aca016b84605f3fd27e499854f558bdf930761e
 branches[hacl-star]=master
 hashes[kremlin]=2cec61205ad7ecb57514d7f2b0f8283d0a22c6c7
