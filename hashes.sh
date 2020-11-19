3b10636b0202aef6353f4cd9a6188b6ca3f42303
Jonathan Protzenko <protz@microsoft.com>
2020-11-19 10:35:29 -0800

    try

--- hashes.sh
+++ hashes.sh
@@ -10,5 +10,5 @@ hashes[mitls-fstar]=c7e0909df4224f902b989d6991ce56866b91b72b
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[quackyducky]=aa0eedac87cb9ba5709e72f1e0a63c8684c44b1a
+hashes[quackyducky]=d7aff3f3
 branches[quackyducky]=master
