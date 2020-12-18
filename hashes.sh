60dbec12376d9cf79be7d55d5b997a5a248cff3c
Dzomo the everest Yak <everbld@microsoft.com>
2020-12-18 05:10:39 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=f7da9460047ebd9d50ce84d66bd3d25ff6b53d77
+hashes[FStar]=99c407222fea38f6e277ebeb96b3abdc706dea86
 branches[FStar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
