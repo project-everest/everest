e1a55615a71852e9899e107388590e833f48f380
Marina Polubelova <polubelovam@gmail.com>
2021-02-04 18:55:12 +0100

    check everest ci with the polubelova_sqr branch

--- hashes.sh
+++ hashes.sh
@@ -2,8 +2,8 @@ declare -A hashes
 declare -A branches
 hashes[FStar]=5d0a057a4b4af4b50406c1a0ed4efafa328cb6fd
 branches[FStar]=master
-hashes[hacl-star]=d5ecb08407b5f949c0338daa4c52ebe16bf8a0bf
-branches[hacl-star]=master
+hashes[hacl-star]=c99dccb288536886ac082d17b9894ad3e4f0b294
+branches[hacl-star]=polubelova_sqr
 hashes[kremlin]=8c76e0dd55c7665b0529b7d64df070ef7aea266d
 branches[kremlin]=master
 hashes[mitls-fstar]=4b2520a321b8e4a1a5d2d1aff79d52d9e8462d47
