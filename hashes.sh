43d9d1fc830c2789d1cfc35adc950f448b5a8a56
Tahina Ramananandro <taramana@microsoft.com>
2021-03-22 13:23:47 -0700

    use F* and kremlin master

--- hashes.sh
+++ hashes.sh
@@ -1,11 +1,11 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=71f544a41ecd1769efcae48bc3e46d1348d4ddd9
-branches[FStar]=_taramana_ppxlib
+hashes[FStar]=3b1b893106c7c0ed8fd054a841d1dc005a71bb99
+branches[FStar]=master
 hashes[hacl-star]=58ae94a1299de375cd5340659a81a43bd9192902
 branches[hacl-star]=master
-hashes[kremlin]=9285c556a313db47ec0d10a9832106972af15763
-branches[kremlin]=wasm-1.1
+hashes[kremlin]=6e60e33aac1551c1ae20e4e02cb66a188935990b
+branches[kremlin]=master
 hashes[mitls-fstar]=f2f31574a58f1ca9ddd138184b988eca20f9e8a9
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
