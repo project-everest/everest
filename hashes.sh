83391d20ce6e0b017eda72209516ddfab443285a
Nikhil Swamy <nikswamy@users.noreply.github.com>
2021-03-22 22:13:27 -0700

    rlimit bump in hacl-star

--- hashes.sh
+++ hashes.sh
@@ -2,8 +2,8 @@ declare -A hashes
 declare -A branches
 hashes[FStar]=e099b844ab808aa9dd9362af80f3cb292be3c62e
 branches[FStar]=nik_wf
-hashes[hacl-star]=58ae94a1299de375cd5340659a81a43bd9192902
-branches[hacl-star]=master
+hashes[hacl-star]=c76125287b461b989d5d81198150738cf4eeccdb
+branches[hacl-star]=nik_wf
 hashes[kremlin]=6e60e33aac1551c1ae20e4e02cb66a188935990b
 branches[kremlin]=master
 hashes[mitls-fstar]=1924efe663b0d78d3e312f799fa0714a7001c483
