92dfac2bf0d93cd18550b6eb98e0f638533887f6
Tahina Ramananandro <taramana@microsoft.com>
2021-03-22 21:52:10 -0700

    upgrade miTLS; reorder hashes

--- hashes.sh
+++ hashes.sh
@@ -1,16 +1,16 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=3b1b893106c7c0ed8fd054a841d1dc005a71bb99
-branches[FStar]=master
-hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
-branches[MLCrypto]=master
 hashes[everquic-crypto]=ff517df9f03a267935c2d5d1f9815a3b2d332325
 branches[everquic-crypto]=master
+hashes[FStar]=3b1b893106c7c0ed8fd054a841d1dc005a71bb99
+branches[FStar]=master
 hashes[hacl-star]=58ae94a1299de375cd5340659a81a43bd9192902
 branches[hacl-star]=master
 hashes[kremlin]=6e60e33aac1551c1ae20e4e02cb66a188935990b
 branches[kremlin]=master
-hashes[mitls-fstar]=20691b463b2fa6282f652a0e287d26ddb952dbe0
+hashes[mitls-fstar]=21fb11869db31ff2c626eef6756dad8e4781f9f2
 branches[mitls-fstar]=dev
+hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
+branches[MLCrypto]=master
 hashes[quackyducky]=aba1310da7d933210fd1e0e6d8ea8b97eb0d9cf9
 branches[quackyducky]=master
