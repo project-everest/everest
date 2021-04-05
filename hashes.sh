e3f1a33512bf00fcf54921af195439973fff03c0
Nikhil Swamy <nikswamy@users.noreply.github.com>
2021-04-05 12:28:29 -0700

    trying nik_decreases on everest

--- hashes.sh
+++ hashes.sh
@@ -1,7 +1,7 @@
 declare -A hashes
 declare -A branches
 hashes[FStar]=2c38adbb55a20f233826593b1c230e2f7865fbc4
-branches[FStar]=master
+branches[FStar]=nik_decreases
 hashes[hacl-star]=2462f98cbf42a606d5f64eab62108b91df4a7b91
 branches[hacl-star]=master
 hashes[kremlin]=6e60e33aac1551c1ae20e4e02cb66a188935990b
