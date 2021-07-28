d1667b536ad2625d55067b9f360e73cf94a69f30
Dzomo the everest Yak <everbld@microsoft.com>
2021-07-28 02:57:45 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -10,5 +10,5 @@ hashes[mitls-fstar]=25f3c6ab65511d41cf71a68e1d431f677e31fe8f
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[quackyducky]=4270e1e0540f663079d7fb293bd19392c3be5b49
+hashes[quackyducky]=d61b5afa052aff422c146e3db297d257802c043d
 branches[quackyducky]=master
