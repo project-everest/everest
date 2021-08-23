57358975e0e97c57072932bda8896d04dd95a41e
Dzomo the everest Yak <everbld@microsoft.com>
2021-08-23 14:58:06 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -2,7 +2,7 @@ declare -A hashes
 declare -A branches
 hashes[FStar]=0a478ef6574b5e0769400a1ca49cc8e83f67894b
 branches[FStar]=master
-hashes[hacl-star]=f57407a82a1ad3422eb3290acdcbb51c3ca7db87
+hashes[hacl-star]=450007868ac5d2260981fa5d6d4776054bcebed1
 branches[hacl-star]=master
 hashes[kremlin]=8d03c65f95fbc79b4f1fcfab1dc4c8f4f985d6d9
 branches[kremlin]=master
