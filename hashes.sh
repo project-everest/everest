9f595633c6741139f182312a914ce8e1cfff5d7e
Dzomo the everest Yak <everbld@microsoft.com>
2021-09-17 05:18:13 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -2,9 +2,9 @@ declare -A hashes
 declare -A branches
 hashes[everquic-crypto]=329730dc0e2989090b44f9aed76846a96d4a8a17
 branches[everquic-crypto]=master
-hashes[FStar]=dbcf9fbbeb79a4da02e5cb19e2b8663f3a9ed26c
+hashes[FStar]=cb9264f27aa77dbe66115b538481e8cfb271f20b
 branches[FStar]=master
-hashes[hacl-star]=02fe1e349550e61762ed3c4fb0e15567ef5bd05b
+hashes[hacl-star]=1ab5f75d593a8bd161e7a4b5630b14e58cbc95d3
 branches[hacl-star]=master
 hashes[kremlin]=3e502374c9afd018dcd062556eb02203dfad1923
 branches[kremlin]=master
@@ -12,5 +12,5 @@ hashes[mitls-fstar]=55d2df071d425b1f256cfa00dfdf62e51e63a5bb
 branches[mitls-fstar]=dev
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[quackyducky]=33d5538d8a92096d39879547a0744983b0fb75a1
+hashes[quackyducky]=ed4aa7016a7356108f7e89d06018a0fe325eae76
 branches[quackyducky]=master
