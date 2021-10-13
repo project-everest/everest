876b56c2866f579d2204b2acac6ca7988199f1bd
Nikhil Swamy <nikswamy@users.noreply.github.com>
2021-10-13 13:37:37 -0700

    try with aseem's hacl patch

--- hashes.sh
+++ hashes.sh
@@ -2,8 +2,8 @@ declare -A hashes
 declare -A branches
 hashes[FStar]=517af8e22ca4c7e488b0c4253dad98211052d1ef
 branches[FStar]=nik_reveal_hide
-hashes[hacl-star]=b415b451e800df6615d897a559d8a865e0952f64
-branches[hacl-star]=master
+hashes[hacl-star]=8b630c2f8f19abd021c0a24cc8a552f5c07e6c38
+branches[hacl-star]=fstar_reveal_hide_normalization
 hashes[kremlin]=bf7d50e85ea4c8cd01bb94a4de2fdf65a9e2ae91
 branches[kremlin]=master
 hashes[mitls-fstar]=2de4877c3286acc9b29d2cbd8adbd41a05466233
