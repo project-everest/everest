c667e3849369a70813e986affc3960e2954107e4
Aseem Rastogi <aseemr@microsoft.com>
2021-10-25 07:54:26 +0000

    update hashes

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=565f73f638053ce1a9eaa63d3b9faa20fbc98af1
+hashes[FStar]=a4afcad5abee0b8ceb4d34e8ec93942a126161fd
 branches[FStar]=_aseem_match_lopt
 hashes[hacl-star]=0129de6a28a3f01e310e93ab45602e16a6b6d7ae
 branches[hacl-star]=master
@@ -10,5 +10,5 @@ hashes[mitls-fstar]=b72503b10141a990f11775358b400d49572b45fb
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[quackyducky]=a08385d5d7cc29377f7a0641cc6175d65d4b56b5
-branches[quackyducky]=master
+hashes[quackyducky]=9cf9588bbfdba845fba7fabcb2b261b757f05488
+branches[quackyducky]=_aseem_match_lopt
