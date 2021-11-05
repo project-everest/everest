7a764db5e08092089677b0db19caa77816f3d608
Nikhil Swamy <nikswamy@users.noreply.github.com>
2021-11-05 16:54:50 -0700

    try again with hacl proof fix

--- hashes.sh
+++ hashes.sh
@@ -2,8 +2,8 @@ declare -A hashes
 declare -A branches
 hashes[FStar]=67e457215bca1004be5e5c379abe196f7234360b
 branches[FStar]=nik_misc
-hashes[hacl-star]=3afacdac4c92423d8553b12c2583955ea1e3a177
-branches[hacl-star]=nik_fstar_issue_2385
+hashes[hacl-star]=4718caa7b73aed832b5749c0c2e2fb91800658c9
+branches[hacl-star]=_nik_fstar_issue_2385
 hashes[kremlin]=bf7d50e85ea4c8cd01bb94a4de2fdf65a9e2ae91
 branches[kremlin]=master
 hashes[mitls-fstar]=d20f58e8579514c3bc8b802531705e8167d5b030
