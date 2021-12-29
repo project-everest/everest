88d68a4be69149918bf233ceda008dc1c4854d00
Jonathan Protzenko <protz@microsoft.com>
2021-12-29 12:05:17 -0800

    try

--- hashes.sh
+++ hashes.sh
@@ -2,12 +2,12 @@ declare -A hashes
 declare -A branches
 hashes[FStar]=4cd5e89442aad653f9cc5b00a32c03b4347f5a93
 branches[FStar]=master
-hashes[hacl-star]=03adadf1c249b0397d7f9acc35c2c66fc4320e23
-branches[hacl-star]=master
-hashes[kremlin]=75d83003d4a055940a6358bbb49a3f65c8dcd9ed
-branches[kremlin]=master
-hashes[mitls-fstar]=d2664765676a491aace2861fd042330b35237e85
-branches[mitls-fstar]=master
+hashes[hacl-star]=d4ea81ff6d1a443a454c8caa79adc0ac5e041599
+branches[hacl-star]=protz_internal_headers
+hashes[kremlin]=0acbfadfbfed17835d85e30a6b852897f9671a9b
+branches[kremlin]=protz_internal_headers
+hashes[mitls-fstar]=74801712d8a5c836ec9d06510693f6776e6c43d4
+branches[mitls-fstar]=protz_internal_headers
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[quackyducky]=51739ea8ba4e5ad1a5e132fc99b737e2cc18173d
