e29e87c658be3962f5d27631a84c13d41683290e
Ubuntu <nswamy@NikDev.pfyjfl5bypxepg2ia0c5fb0zmc.bx.internal.cloudapp.net>
2022-01-24 14:11:33 +0000

    try with eq3 update

--- hashes.sh
+++ hashes.sh
@@ -1,9 +1,9 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=c50cff2dbdb7368681a4f1bac07f7e8588bc1dfd
-branches[FStar]=master
-hashes[hacl-star]=6d8bb39a8cdd2a40ed60137898fe5c492c10c7bc
-branches[hacl-star]=protz_remove_benchmarks
+hashes[FStar]=6fd6231cebe6870e7d7d37d36c34f0ce41fb8796
+branches[FStar]=nik_misc
+hashes[hacl-star]=f4373adebe3d73e9fc3cfb447fad3496892432b8
+branches[hacl-star]=nik_eq3
 hashes[kremlin]=fadabccc919c25bcf715a2ed0736a69dcc7d6589
 branches[kremlin]=master
 hashes[mitls-fstar]=c6e1d006dfe0b44ec787dbb366a08924a26a8ba2
