28c113976e4c70788aa0648179135656ecdc7a6a
Nikhil Swamy <nikswamy@users.noreply.github.com>
2022-03-18 20:27:33 +0000

    try with hacl patch

--- hashes.sh
+++ hashes.sh
@@ -2,8 +2,8 @@ declare -A hashes
 declare -A branches
 hashes[FStar]=2bebc089be4600d9df9902f75c227c23e84ec8c6
 branches[FStar]=nik_misc
-hashes[hacl-star]=b163f0972500847c88adc7f4bbe1425c1867e8c9
-branches[hacl-star]=master
+hashes[hacl-star]=723b2962b67d4eceb719ac946811fe409e797fb3
+branches[hacl-star]=_nik_misc
 hashes[kremlin]=5bf80a8fdf259b0fab47fd6e9d2dae4a8d9f8479
 branches[kremlin]=master
 hashes[mitls-fstar]=a80e8ed001241e67e76222b4f9181b6960cd5824
