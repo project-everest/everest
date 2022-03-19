91e43d586fb0400f023678f4d5c419d39ae62f4f
Nikhil Swamy <nikswamy@users.noreply.github.com>
2022-03-19 17:22:35 +0000

    try with everparse patch

--- hashes.sh
+++ hashes.sh
@@ -10,5 +10,5 @@ hashes[mitls-fstar]=a80e8ed001241e67e76222b4f9181b6960cd5824
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[quackyducky]=95754a8d79bdc28891afd274d9eba292e8e583d5
-branches[quackyducky]=master
+hashes[quackyducky]=cd89a4fb8e0ac02586e2a4850c47da45f756f290
+branches[quackyducky]=_nik_misc
