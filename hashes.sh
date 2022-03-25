4c08dedd675b1e86da52e216bbdfd201a43f5303
Tahina Ramananandro <taramana@microsoft.com>
2022-03-25 13:48:17 -0700

    update

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[everquic-crypto]=ae0ec93ef39a86d46cd811e2e4d6732a5ba01f05
+hashes[everquic-crypto]=21627945ee7210a2570dfd7eb8ef5516af5f67a7
 branches[everquic-crypto]=master
 hashes[FStar]=75df2b47a7f3f23bc5266f750904f775f27a0be2
 branches[FStar]=master
@@ -8,7 +8,7 @@ hashes[hacl-star]=79f1c0c8dded78e1dd92795a0bb1f8a4a5c92bee
 branches[hacl-star]=master
 hashes[kremlin]=d9da0195a79b1ee0cdd668c1bc644dd55e08723a
 branches[kremlin]=master
-hashes[mitls-fstar]=6d5c61d36421080a9ba11b0fbaf0555599182b66
+hashes[mitls-fstar]=f26b4a712b7b08f4924ad7f16c9361a0dab8c41b
 branches[mitls-fstar]=dev
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
