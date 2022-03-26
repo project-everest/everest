013b4e6dcea6b562ff561e20276507718c5c3eea
Tahina Ramananandro <taramana@microsoft.com>
2022-03-25 22:53:26 -0700

    krml rename: scripts

--- repositories.sh
+++ repositories.sh
@@ -4,8 +4,8 @@ declare -A https
 repositories[FStar]=git@github.com:FStarLang/FStar.git
 https[FStar]=https://github.com/FStarLang/FStar.git
 
-repositories[kremlin]=git@github.com:FStarLang/kremlin.git
-https[kremlin]=https://github.com/FStarLang/kremlin.git
+repositories[karamel]=git@github.com:FStarLang/karamel.git
+https[karamel]=https://github.com/FStarLang/karamel.git
 
 repositories[hacl-star]=git@github.com:project-everest/hacl-star.git
 https[hacl-star]=https://github.com/project-everest/hacl-star.git
