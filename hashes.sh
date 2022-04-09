3914ab2704f2424e5630a7fa8787e8141c38fc3f
Dzomo the everest Yak <everbld@microsoft.com>
2022-04-09 05:42:10 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[everparse]=58b3a64fb19421c5234d4b1d5d79c1b33599fcf4
+hashes[everparse]=5f975d673d2e0a18bd77c8e8d892602edf9c9656
 branches[everparse]=master
 hashes[everquic-crypto]=1101b523315d30564f62cfb4c7737dc26bb04390
 branches[everquic-crypto]=master
-hashes[FStar]=2c8749b644dce45ee75ddff1f601e06cee1cffd9
+hashes[FStar]=2d4211e603f70a0e287a5c9d27e2560bec83c0da
 branches[FStar]=master
-hashes[hacl-star]=adc8c664f10d643b04e40c17727ab25e8f8f763b
+hashes[hacl-star]=02a8ea02bb2af96ac54e230d41465875d6959803
 branches[hacl-star]=master
 hashes[karamel]=70084646b6f5cf01d58a2ed1c0e69d34829ba480
 branches[karamel]=master
