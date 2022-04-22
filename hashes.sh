9f70c0275cf5740a3b2b93ce71d692a1381aa13d
Nikhil Swamy <nikswamy@users.noreply.github.com>
2022-04-22 18:40:55 +0000

    bump nik_typeclasses

--- hashes.sh
+++ hashes.sh
@@ -2,7 +2,7 @@ declare -A hashes
 declare -A branches
 hashes[everparse]=428eefe5fafd80a3fdddcf2f4ed98b82b18e6bfe
 branches[everparse]=master
-hashes[FStar]=ba0bd60f3277e3fedaace8442ea3b74618412251
+hashes[FStar]=161ac8f81811835d616db06b49219c623b331c93
 branches[FStar]=nik_typeclasses
 hashes[hacl-star]=73d33120c8e2ccd1001acc27ce23843e96aa3aae
 branches[hacl-star]=master
