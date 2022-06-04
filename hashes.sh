295406128d99b2e83d425cfa8c43ceb4825fa28c
Dzomo, the Everest Yak <everbld@microsoft.com>
2022-06-04 01:22:02 +0100

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -2,9 +2,9 @@ declare -A hashes
 declare -A branches
 hashes[everparse]=e7227152dfade6cb4f0adf7dc03816273c0ef4e0
 branches[everparse]=master
-hashes[FStar]=c406bf3d397e00491aa58ed02c89b14232767286
+hashes[FStar]=96534f492dd4a91a78ad5d01e9c39e49808c1c10
 branches[FStar]=master
-hashes[hacl-star]=e2a939cced1d71b3984d1be775e4a9f6d6125b24
+hashes[hacl-star]=49c852f9b8bd67a208e402f97c79092953f4d504
 branches[hacl-star]=master
 hashes[karamel]=d67210df9520c3b1705dd540acf891b8030f73a8
 branches[karamel]=master
