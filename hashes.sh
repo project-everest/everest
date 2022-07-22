7d23a4f3f9c61ebb1ac454b09a16939c99ebe0fc
Jonathan Protzenko <protz@microsoft.com>
2022-07-22 11:47:43 -0700

    try

--- hashes.sh
+++ hashes.sh
@@ -2,13 +2,13 @@ declare -A hashes
 declare -A branches
 hashes[everparse]=b770e9b7153a18c05b481957594ac99a86403a1c
 branches[everparse]=master
-hashes[FStar]=9988091552ae4fd0786240fc4b116fc3546450fc
+hashes[FStar]=5bcd16865e5161ccbc7b2b9ead96ba099618adf8
 branches[FStar]=master
 hashes[hacl-star]=1cf18e5dd0d7d791d352cfe3c7faa14b9cb602a6
 branches[hacl-star]=protz_fix_dependency_edge
-hashes[karamel]=ebbf6437d8f969ce4459ffe7479f6014830df308
+hashes[karamel]=4e9212d13f0747c82a8d2702afe7e1c1411be589
 branches[karamel]=protz_monomorphization
-hashes[mitls-fstar]=d0c3a4419627c088ee209c732f86efd948380dc6
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=0196435f6a68b0aecf2828184a9b5b3266d64aa6
+branches[mitls-fstar]=protz_monomorphization
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
