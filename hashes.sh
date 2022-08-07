b3413613fb3a3221b5c610545e5c68e1c96f881b
Tahina Ramananandro <taramana@microsoft.com>
2022-08-07 15:44:58 -0700

    update everquic-crypto

--- hashes.sh
+++ hashes.sh
@@ -2,7 +2,7 @@ declare -A hashes
 declare -A branches
 hashes[everparse]=4f19cc5c8a12c0602984e7462b9c9feb1a9419c6
 branches[everparse]=master
-hashes[everquic-crypto]=be1897c4d14731aceef0c7aa00e94607e57a0ff9
+hashes[everquic-crypto]=16d7536235693258ab7fb3444ca35b70bee3f3f5
 branches[everquic-crypto]=master
 hashes[FStar]=36103a995e9642bf8c12440269b40207355938c4
 branches[FStar]=master
