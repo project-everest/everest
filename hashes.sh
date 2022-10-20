73ba98708e03766e4db04d3d63cf396206e41c2e
Jonathan Protzenko <protz@microsoft.com>
2022-10-20 14:46:20 -0700

    try

--- hashes.sh
+++ hashes.sh
@@ -2,12 +2,12 @@ declare -A hashes
 declare -A branches
 hashes[everparse]=be96e489c05dfb059b2f54cb707ec08f47bd720f
 branches[everparse]=master
-hashes[everquic-crypto]=68ae95ed536909f1dc70c1f338993f610c2cc62f
-branches[everquic-crypto]=master
+hashes[everquic-crypto]=3d1768e56aa1fdf94f6b99bb3e7e8c9f578c233c
+branches[everquic-crypto]=protz_relocate_evercrypt_ctr
 hashes[FStar]=2d389c6a740c35004a500f426ac92a5c2e9bec91
 branches[FStar]=master
-hashes[hacl-star]=cd82cb8391f48e819fac229d32c6cacbefd11cc5
-branches[hacl-star]=main
+hashes[hacl-star]=850e7c0efd14642087c6ab911ea08ac75bfa935d
+branches[hacl-star]=protz_relocate_evercrypt_ctr
 hashes[karamel]=e0a41f4e120e4f3e1c08090856a05f3e208edf29
 branches[karamel]=master
 hashes[mitls-fstar]=a12125f8ffa63596bc50720853e4d0c0564f45e9
