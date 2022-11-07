6ce1c7486ae757214887c03bc54db08044d24e08
Aseem Rastogi <aseemr@microsoft.com>
2022-11-07 06:01:48 +0000

    hashes

--- hashes.sh
+++ hashes.sh
@@ -1,11 +1,11 @@
 declare -A hashes
 declare -A branches
-hashes[everparse]=4ee776235d38de28c03301063b9d2447dfcdacea
-branches[everparse]=master
+hashes[everparse]=4a85ac6ecbd6ba1e8a38a0c62c2799cc204293d4
+branches[everparse]=_fstar_core_tc
 hashes[everquic-crypto]=deeb1137ed863c5949b4e521923c44490f42617c
 branches[everquic-crypto]=master
-hashes[FStar]=9dfeeb93f6e74fb5f930d35414f187a3806f0bbf
-branches[FStar]=master
+hashes[FStar]=52c4fdbe0e55e1c956fad27319c93855bb6452e8
+branches[FStar]=_core_typechecker
 hashes[hacl-star]=51a72a953a4ee6f91e63b2816ae5c4e62edf35d6
 branches[hacl-star]=main
 hashes[karamel]=6dd219f468907553b65cda0cdb094eae849cf773
