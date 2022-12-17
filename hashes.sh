95e553533ec92772a479f1346b26930477961ea3
Jonathan Protzenko <protz@microsoft.com>
2022-12-16 21:55:46 -0800

    try

--- hashes.sh
+++ hashes.sh
@@ -1,18 +1,18 @@
 declare -A hashes
 declare -A branches
-hashes[everparse]=8e4058c1cb26c99ada9c8c02ec1224d5a4b34a8e
+hashes[everparse]=be96e489c05dfb059b2f54cb707ec08f47bd720f
 branches[everparse]=master
 hashes[everquic-crypto]=deeb1137ed863c5949b4e521923c44490f42617c
 branches[everquic-crypto]=master
-hashes[FStar]=13dbe4866835037a0721f6aa9521bb13fbda5c16
+hashes[FStar]=9310344fa634db700ef2317ca77c4c21004a8c3f
 branches[FStar]=master
-hashes[hacl-star]=80e8072675
-branches[hacl-star]=main
-hashes[karamel]=6f3f060ed4b3c597728cc938986825946c621b14
-branches[karamel]=master
+hashes[hacl-star]=3eed079dec17ea6227e64af59bed4f65aa0aadca
+branches[hacl-star]=protz_static_internal
+hashes[karamel]=489ac00387fc3477d438eb0a07829d396fd93a2a
+branches[karamel]=protz_static_internal
+hashes[merkle-tree]=19b1307e74973a2f49deca271e047e30dc50a170
+branches[merkle-tree]=main
 hashes[mitls-fstar]=a12125f8ffa63596bc50720853e4d0c0564f45e9
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[merkle-tree]=b7be609e0810835e2a72eddacd4ba866bdc9d4c9
-branches[merkle-tree]=main
