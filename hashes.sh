4885e137dbd35818883220c9f63a01b3183c7fd2
Tahina Ramananandro <taramana@microsoft.com>
2023-03-02 16:34:53 -0800

    update to latest HACL*, EverParse

--- hashes.sh
+++ hashes.sh
@@ -1,12 +1,12 @@
 declare -A hashes
 declare -A branches
-hashes[everparse]=d9a5f5f39729434c57fb8d3382b9a41e6f701293
+hashes[everparse]=eebfa61d43e8144fd9b1e8731195f71ca40d7908
 branches[everparse]=master
 hashes[everquic-crypto]=deeb1137ed863c5949b4e521923c44490f42617c
 branches[everquic-crypto]=master
 hashes[FStar]=eb911fc41d5ba730c15f2ac296ffc5ebf7b46560
 branches[FStar]=master
-hashes[hacl-star]=6c3b25f82ada299aa7bdce41d4d6207f4b0d7b95
+hashes[hacl-star]=b8dea01968a8de80c27ca8b09daf619eba0d7d44
 branches[hacl-star]=main
 hashes[karamel]=bd359d86d59401b001788468fa7366e741836c02
 branches[karamel]=master
