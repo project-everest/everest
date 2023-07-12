79b0803b3078de1b712c64ec0205c10dbe4af2ef
Dzomo, the Everest Yak <everbld@microsoft.com>
2023-07-12 11:40:31 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -16,5 +16,5 @@ hashes[mitls-fstar]=208725c811e965007d29dce57df05f25550fd1b5
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[steel]=7343ac2c44bdd0fb2adccabdac9ee76068b4875a
+hashes[steel]=27404a024a2e8943e24c6a9d30a4de17a0c992e4
 branches[steel]=main
