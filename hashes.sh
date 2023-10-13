cdf75bca38bdbb448263ae5b48dbfae82d4e42ba
Tahina Ramananandro <taramana@microsoft.com>
2023-10-13 14:06:42 -0700

    test FStarLang/karamel#385 with project-everest/everparse#104

--- hashes.sh
+++ hashes.sh
@@ -1,15 +1,15 @@
 declare -A hashes
 declare -A branches
-hashes[everparse]=cc5923f1c39fe7957dbf65537e697047022ce504
-branches[everparse]=master
+hashes[everparse]=04d63559375069362d7738b2ed5c8d9fdbe34c45
+branches[everparse]=_taramana_krml_385
 hashes[everquic-crypto]=76c14ada5db16b85a33920d826df0552e4c3cf10
 branches[everquic-crypto]=master
 hashes[FStar]=bc622701c668f6b4092760879372968265d4a4e1
 branches[FStar]=master
 hashes[hacl-star]=1b30697fc2b0d8d5e2f541eccfd3fb52b45b905c
 branches[hacl-star]=main
-hashes[karamel]=da83fc312e2f3833f5746dfb702a456048a7a3da
-branches[karamel]=master
+hashes[karamel]=e2fee13cf92daa08b45471a2cc87523034c134cb
+branches[karamel]=protz_ignore_ifdef
 hashes[merkle-tree]=3b0f086655c145aa23f58a97259ebf4cf112a4a3
 branches[merkle-tree]=main
 hashes[mitls-fstar]=148206939f9897901d10a6b105044539d04d702a
