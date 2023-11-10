ec55bf746bcb69cee4dd3ab8423761c002b09ba4
Dzomo, the Everest Yak <everbld@microsoft.com>
2023-11-10 06:24:59 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -2,7 +2,7 @@ declare -A hashes
 declare -A branches
 hashes[everparse]=01760c8adc769e194e7db8753f9dea7b9ddb26a4
 branches[everparse]=master
-hashes[everquic-crypto]=76c14ada5db16b85a33920d826df0552e4c3cf10
+hashes[everquic-crypto]=cc4457e546cdb1e8f9a23d2e4fa387aab1a6b502
 branches[everquic-crypto]=master
 hashes[FStar]=9b60a5b758581edb32488a13e11039db199f89e1
 branches[FStar]=master
@@ -10,7 +10,7 @@ hashes[hacl-star]=e1a785f7e88bb6668c9beabd4dc292a03c68bb2e
 branches[hacl-star]=main
 hashes[karamel]=a7be2a7c43eca637ceb57fe8f3ffd16fc6627ebd
 branches[karamel]=master
-hashes[merkle-tree]=3b0f086655c145aa23f58a97259ebf4cf112a4a3
+hashes[merkle-tree]=8d46446a3bd81e16a1b4dd694cab94ecfa9e6a0f
 branches[merkle-tree]=main
 hashes[mitls-fstar]=756f89ccbecf9a38a07378c1ce46bf6e180a183d
 branches[mitls-fstar]=master
