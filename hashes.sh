3fdd6cce8dd91cc409f5796d703a873c00165927
Dzomo, the Everest Yak <everbld@microsoft.com>
2024-01-23 01:14:15 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[everparse]=34e7239db9e6ee73ac2af44dcff8cf0fdeb70ff0
+hashes[everparse]=00217c4a89f5ba56002ba9aa5b4a9d5903bfe9fa
 branches[everparse]=master
 hashes[everquic-crypto]=cc4457e546cdb1e8f9a23d2e4fa387aab1a6b502
 branches[everquic-crypto]=master
