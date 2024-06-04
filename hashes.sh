ba13dcff45ff8b70cc89293bfd83f472f52ed28c
Dzomo, the Everest Yak <24394600+dzomo@users.noreply.github.com>
2024-06-04 18:43:17 +0000

    [CI] automatic upgrade

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[everparse]=b5f6c1f16017daa709b4d056526a00413bb1d9f3
+hashes[everparse]=0fbed501ff66a3bc9a3c9eb57df467ac3adeefba
 branches[everparse]=master
 hashes[everquic-crypto]=c773c8e7de1bb51369561723e9cae618339e162d
 branches[everquic-crypto]=master
@@ -8,11 +8,11 @@ hashes[FStar]=9820798dcc31cd1ea5c164611a67f58ade0b7655
 branches[FStar]=master
 hashes[hacl-star]=5ec960b861e3d7f2d29f8f0112034dda8a2ae6db
 branches[hacl-star]=main
-hashes[karamel]=1b809c2917853d1117034c78d978ac19642aa84a
+hashes[karamel]=40e3a60397e796e3c45a63b558c60b90b8e425ec
 branches[karamel]=master
 hashes[merkle-tree]=99fc1dcf0de8278fbe440e07a892226f5521f38c
 branches[merkle-tree]=main
-hashes[mitls-fstar]=303cb7a8cd41ee3a488b6f499d319c033192ecb1
+hashes[mitls-fstar]=e9805d0173eb6f8addcca6ea8ad4e7c8f2046689
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
