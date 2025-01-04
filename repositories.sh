9b3072983b03b596dc802bb85943f2945a5c6e58
Guido Martínez <mtzguido@gmail.com>
2025-01-04 11:06:38 -0800

    Point everything to dev forks

--- repositories.sh
+++ repositories.sh
@@ -1,33 +1,33 @@
 declare -A repositories
 declare -A https
 
-repositories[everparse]=git@github.com:project-everest/everparse.git
-https[everparse]=https://github.com/project-everest/everparse.git
+repositories[everparse]=git@github.com:mtzguido/everparse.git
+https[everparse]=https://github.com/mtzguido/everparse.git
 
-repositories[FStar]=git@github.com:FStarLang/FStar.git
-https[FStar]=https://github.com/FStarLang/FStar.git
+repositories[FStar]=git@github.com:mtzguido/FStar.git
+https[FStar]=https://github.com/mtzguido/FStar.git
 
-repositories[karamel]=git@github.com:FStarLang/karamel.git
-https[karamel]=https://github.com/FStarLang/karamel.git
+repositories[karamel]=git@github.com:mtzguido/karamel.git
+https[karamel]=https://github.com/mtzguido/karamel.git
 
-repositories[steel]=git@github.com:FStarLang/steel.git
-https[steel]=https://github.com/FStarLang/steel.git
+repositories[steel]=git@github.com:mtzguido/steel.git
+https[steel]=https://github.com/mtzguido/steel.git
 
-repositories[hacl-star]=git@github.com:hacl-star/hacl-star.git
-https[hacl-star]=https://github.com/hacl-star/hacl-star.git
+repositories[hacl-star]=git@github.com:mtzguido/hacl-star.git
+https[hacl-star]=https://github.com/mtzguido/hacl-star.git
 
-repositories[merkle-tree]=git@github.com:hacl-star/merkle-tree.git
-https[merkle-tree]=https://github.com/hacl-star/merkle-tree.git
+repositories[merkle-tree]=git@github.com:mtzguido/merkle-tree.git
+https[merkle-tree]=https://github.com/mtzguido/merkle-tree.git
 
-repositories[mitls-fstar]=git@github.com:project-everest/mitls-fstar.git
-https[mitls-fstar]=https://github.com/project-everest/mitls-fstar.git
+repositories[mitls-fstar]=git@github.com:mtzguido/mitls-fstar.git
+https[mitls-fstar]=https://github.com/mtzguido/mitls-fstar.git
 
 repositories[MLCrypto]=git@github.com:project-everest/MLCrypto.git
 https[MLCrypto]=https://github.com/project-everest/MLCrypto.git
 
-repositories[pulse]=git@github.com:FStarLang/pulse
-https[pulse]=https://github.com/FStarLang/pulse
+repositories[pulse]=git@github.com:mtzguido/pulse
+https[pulse]=https://github.com/mtzguido/pulse
 
-repositories[everquic-crypto]=git@github.com:project-everest/everquic-crypto.git
-https[everquic-crypto]=https://github.com/project-everest/everquic-crypto.git
+repositories[everquic-crypto]=git@github.com:mtzguido/everquic-crypto.git
+https[everquic-crypto]=https://github.com/mtzguido/everquic-crypto.git
 
